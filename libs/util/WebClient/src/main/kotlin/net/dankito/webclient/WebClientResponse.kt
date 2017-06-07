3503c9a32b55609ada1ade0d5697ea21572a7b49
dankl <dankl@comfylight.com>
2017-06-08 00:03:09 +0200

    Made WebClientResponse a data class

--- libs/util/WebClient/src/main/kotlin/net/dankito/webclient/WebClientResponse.kt
+++ libs/util/WebClient/src/main/kotlin/net/dankito/webclient/WebClientResponse.kt
@@ -1,6 +1,4 @@
 package net.dankito.webclient
 
 
-class WebClientResponse(val isSuccessful: Boolean, val error: Exception? = null, val body: String? = null) {
-
-}
+data class WebClientResponse(val isSuccessful: Boolean, val error: Exception? = null, val body: String? = null)
