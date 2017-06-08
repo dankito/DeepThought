c55d17554f536f8e4c70f5717d1b8880665f0ed3
dankl <dankl@comfylight.com>
2017-06-08 14:49:52 +0200

    Applied correct package name

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/feed/FeedArticleSummaryExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/feed/FeedArticleSummaryExtractor.kt
@@ -1,8 +1,8 @@
-package net.dankito.newsreader.summary
+package net.dankito.newsreader.feed
 
-import net.dankito.newsreader.feed.IFeedReader
-import net.dankito.newsreader.model.ArticleSummary
 import net.dankito.data_access.network.webclient.extractor.AsyncResult
+import net.dankito.newsreader.model.ArticleSummary
+import net.dankito.newsreader.summary.IArticleSummaryExtractor
 
 
 class FeedArticleSummaryExtractor(private val feedUrl : String, private val feedReader: IFeedReader) : IArticleSummaryExtractor {
