e7043c522d81156b929672f3f297bf08f371bcbd
dankl <dankl@comfylight.com>
2017-06-08 20:26:47 +0200

    Implemented Router

---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/routing/Router.kt
@@ -0,0 +1,36 @@
+package net.dankito.deepthought.android.routing
+
+import android.content.Context
+import android.content.Intent
+import net.dankito.deepthought.android.activities.ArticleSummaryActivity
+import net.dankito.deepthought.android.activities.ViewArticleActivity
+import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfig
+import net.dankito.newsreader.model.Article
+import net.dankito.serializer.ISerializer
+import net.dankito.serializer.JacksonJsonSerializer
+
+
+class Router(private val context: Context) {
+
+    private val serializer: ISerializer = JacksonJsonSerializer() // TODO: inject
+
+
+    fun showArticleSummaryView(extractor: ArticleSummaryExtractorConfig) {
+        val articleSummaryActivityIntent = Intent(context, ArticleSummaryActivity::class.java)
+
+        articleSummaryActivityIntent.putExtra(ArticleSummaryActivity.EXTRACTOR_URL_INTENT_EXTRA_NAME, extractor.url)
+
+        context.startActivity(articleSummaryActivityIntent)
+    }
+
+    fun showArticleView(article: Article) {
+        val serializedArticle = serializer.serializeObject(article)
+
+        val viewArticleIntent = Intent(context, ViewArticleActivity::class.java)
+
+        viewArticleIntent.putExtra(ViewArticleActivity.ARTICLE_INTENT_EXTRA_NAME, serializedArticle)
+
+        context.startActivity(viewArticleIntent)
+    }
+
+}
