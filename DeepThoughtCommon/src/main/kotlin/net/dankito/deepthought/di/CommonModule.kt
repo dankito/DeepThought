671362aa9041434a3569704b25904da9b8ae0eb3
dankl <dankl@comfylight.com>
2017-06-08 23:28:26 +0200

    Added provideImageCache(), provideArticleSummaryExtractorConfigManager() and provideArticleExtractors()

--- DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/di/CommonModule.kt
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/di/CommonModule.kt
@@ -2,12 +2,16 @@ package net.dankito.deepthought.di
 
 import dagger.Module
 import dagger.Provides
+import net.dankito.data_access.filesystem.IFileStorageService
 import net.dankito.data_access.network.webclient.IWebClient
 import net.dankito.data_access.network.webclient.OkHttpWebClient
+import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfigManager
 import net.dankito.faviconextractor.FaviconComparator
 import net.dankito.faviconextractor.FaviconExtractor
+import net.dankito.newsreader.article.ArticleExtractors
 import net.dankito.serializer.ISerializer
 import net.dankito.serializer.JacksonJsonSerializer
+import net.dankito.utils.ImageCache
 import javax.inject.Singleton
 
 
@@ -30,6 +34,13 @@ class CommonModule {
 
     @Provides
     @Singleton
+    fun provideImageCache(webClient: IWebClient, serializer: ISerializer, fileStorageService: IFileStorageService) : ImageCache {
+        return ImageCache(webClient,serializer, fileStorageService)
+    }
+
+
+    @Provides
+    @Singleton
     fun provideFaviconExtractor(webClient: IWebClient) : FaviconExtractor {
         return FaviconExtractor(webClient)
     }
@@ -38,6 +49,19 @@ class CommonModule {
     @Singleton
     fun provideFaviconComparator(webClient: IWebClient) : FaviconComparator {
         return FaviconComparator(webClient)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideArticleSummaryExtractorConfigManager(webClient: IWebClient, fileStorageService: IFileStorageService) : ArticleSummaryExtractorConfigManager {
+        return ArticleSummaryExtractorConfigManager(webClient, fileStorageService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideArticleExtractors(webClient: IWebClient) : ArticleExtractors {
+        return ArticleExtractors(webClient)
     }
 
 }
