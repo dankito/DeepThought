f07948d4178e35751f9395e735d8fa4a750fc65b
dankl <dankl@comfylight.com>
2017-06-09 21:36:00 +0200

    Providing IEntityManager

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/di/AndroidCommonModule.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/di/AndroidCommonModule.kt
@@ -3,6 +3,9 @@ package net.dankito.deepthought.android.di
 import android.content.Context
 import dagger.Module
 import dagger.Provides
+import net.dankito.data_access.database.AndroidCouchbaseLiteEntityManager
+import net.dankito.data_access.database.EntityManagerConfiguration
+import net.dankito.data_access.database.IEntityManager
 import net.dankito.data_access.filesystem.AndroidFileStorageService
 import net.dankito.data_access.filesystem.IFileStorageService
 import javax.inject.Singleton
@@ -11,6 +14,16 @@ import javax.inject.Singleton
 @Module
 class AndroidCommonModule {
 
+
+    @Provides
+    @Singleton
+    fun provideEntityManager(context: Context, configuration: EntityManagerConfiguration) : IEntityManager {
+        val entityManager = AndroidCouchbaseLiteEntityManager(context)
+
+        entityManager.open(configuration)
+
+        return entityManager
+    }
 
     @Provides
     @Singleton
