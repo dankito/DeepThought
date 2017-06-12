1fa0fdb0f33f2eea0a95e558d4bb0997e4571138
dankl <dankl@comfylight.com>
2017-06-12 19:46:02 +0200

    Implemented LazyLoadingLuceneSearchResultsList

---
+++ libs/service_layer/search/LuceneSearchEngine/src/main/kotlin/net/dankito/service/search/results/LazyLoadingLuceneSearchResultsList.kt
@@ -0,0 +1,69 @@
+package net.dankito.service.search.results
+
+import net.dankito.data_access.database.IEntityManager
+import net.dankito.deepthought.model.BaseEntity
+import net.dankito.service.search.SortOption
+import net.dankito.service.search.SortOrder
+import net.dankito.service.search.util.LazyLoadingList
+import org.apache.lucene.search.*
+import org.slf4j.LoggerFactory
+import java.util.*
+
+
+class LazyLoadingLuceneSearchResultsList<T : BaseEntity>(entityManager: IEntityManager, private var searcher: IndexSearcher, query: Query, resultType: Class<T>,
+                             private var idFieldName: String, countTopNHits: Int = 1000, sortOptions: List<SortOption> = ArrayList<SortOption>(0))
+    : LazyLoadingList<T>(entityManager, resultType) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(LazyLoadingLuceneSearchResultsList::class.java)
+    }
+
+
+    init {
+        try {
+            val sort = getSorting(sortOptions)
+            val hits = searcher.search(query, countTopNHits, sort).scoreDocs
+
+            this.entityIds = retrieveEntityIds(hits)
+        } catch (ex: Exception) {
+            log.error("Could not execute Query " + query, ex)
+        }
+
+    }
+
+    private fun getSorting(sortOptions: List<SortOption>): Sort {
+        val sort = Sort()
+
+        if (sortOptions.size > 0) {
+            val sortFields = arrayOfNulls<SortField>(sortOptions.size)
+
+            for(i in sortOptions.indices) {
+                val (fieldName, order, type) = sortOptions[i]
+
+                sortFields[i] = SortField(fieldName, type, order === SortOrder.Descending)
+            }
+
+            sort.setSort(*sortFields)
+        }
+
+        return sort
+    }
+
+    private fun retrieveEntityIds(hits: Array<ScoreDoc>): MutableCollection<String> {
+        val ids = LinkedHashSet<String>()
+
+        try {
+            for(index in hits.indices) {
+                val hitDoc = searcher.doc(hits[index].doc)
+                val entityId = hitDoc.getField(idFieldName).stringValue()
+
+                ids.add(entityId)
+            }
+        } catch (ex: Exception) {
+            log.error("Could not get all Entity IDs from Lucene Search Result", ex)
+        }
+
+        return ids
+    }
+
+}
