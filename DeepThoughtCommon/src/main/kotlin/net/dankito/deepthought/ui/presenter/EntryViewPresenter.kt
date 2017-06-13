d69119803d3809f2495a022ae49f7086292aba45
dankl <dankl@comfylight.com>
2017-06-13 10:00:55 +0200

    Extracted EntryViewPresenter from ViewEntryActivity

---
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/EntryViewPresenter.kt
@@ -0,0 +1,41 @@
+package net.dankito.deepthought.ui.presenter
+
+import net.dankito.deepthought.model.Entry
+import net.dankito.deepthought.model.Reference
+import net.dankito.deepthought.ui.IRouter
+import net.dankito.newsreader.model.EntryExtractionResult
+import net.dankito.service.data.EntryService
+import net.dankito.service.data.ReferenceService
+
+
+class EntryViewPresenter(private var entry: Entry?, private var entryExtractionResult: EntryExtractionResult?,
+                         private var entryService: EntryService, private var referenceService: ReferenceService, private var router: IRouter) {
+
+    fun saveEntry() {
+        entryExtractionResult?.let { entryExtractionResult ->
+            val entry = entryExtractionResult.entry
+
+            entry.reference = createAndPersistReference(entryExtractionResult)
+
+            entryService.persist(entry)
+        }
+
+        returnToPreviousView()
+    }
+
+    private fun createAndPersistReference(entryExtractionResult: EntryExtractionResult): Reference? {
+        val reference = entryExtractionResult.reference
+
+        if(reference != null) {
+            referenceService.persist(reference)
+        }
+
+        return reference
+    }
+
+
+    fun returnToPreviousView() {
+        router.returnToPreviousView()
+    }
+
+}
