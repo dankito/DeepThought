30d0e8722dd84d76bb96149ac34e256afa32f4d0
dankl <dankl@comfylight.com>
2017-06-15 01:20:19 +0200

    Fixed return type of provideRouter()

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/di/ActivitiesModule.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/di/ActivitiesModule.kt
@@ -6,6 +6,7 @@ import dagger.Provides
 import net.dankito.deepthought.android.DeepThoughtApplication
 import net.dankito.deepthought.android.routing.AndroidRouter
 import net.dankito.deepthought.android.service.ui.CurrentActivityTracker
+import net.dankito.deepthought.ui.IRouter
 import net.dankito.serializer.ISerializer
 import javax.inject.Singleton
 
@@ -29,7 +30,7 @@ class ActivitiesModule(private val application: DeepThoughtApplication) {
 
     @Provides
     @Singleton
-    fun provideRouter(context: Context, activityTracker: CurrentActivityTracker, serializer: ISerializer) : AndroidRouter {
+    fun provideRouter(context: Context, activityTracker: CurrentActivityTracker, serializer: ISerializer) : IRouter {
         return AndroidRouter(context, activityTracker, serializer)
     }
 
