dde11b31ac27e37eca493ffbe96ec64f063d72d4
dankl <dankl@comfylight.com>
2017-06-16 01:59:21 +0200

    Removed unused AssociationEntity

--- libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/AssociationEntity.kt
+++
@@ -1,9 +0,0 @@
-package net.dankito.deepthought.model
-
-import javax.persistence.MappedSuperclass
-
-/**
- * Just a dummy class to mark Association Entities. AssociationEntities contrary to all other Entities really get deleted from Database instead just setting their deleted flag to true.
- */
-@MappedSuperclass
-class AssociationEntity : BaseEntity()
