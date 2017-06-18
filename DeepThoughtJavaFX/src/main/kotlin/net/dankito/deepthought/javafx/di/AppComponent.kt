517f2bca1def5f30bc679cb8811af2f11699f65d
dankl <dankl@comfylight.com>
2017-06-18 23:45:32 +0200

    Added AppComponent

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/di/AppComponent.kt
@@ -0,0 +1,27 @@
+package net.dankito.deepthought.javafx.di
+
+import dagger.Component
+import net.dankito.deepthought.di.CommonComponent
+import net.dankito.deepthought.di.CommonDataModule
+import net.dankito.deepthought.di.CommonModule
+import net.dankito.deepthought.java.service.di.JavaCommonModule
+import net.dankito.deepthought.javafx.DeepThoughtJavaFXApplication
+import net.dankito.deepthought.javafx.dialogs.mainwindow.MainWindow
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(modules = arrayOf(JavaFXModule::class, JavaCommonModule::class, CommonModule::class, CommonDataModule::class))
+interface AppComponent : CommonComponent {
+
+    companion object {
+        lateinit var component: AppComponent
+    }
+
+
+
+    fun inject(app: DeepThoughtJavaFXApplication)
+
+    fun inject(mainWindow: MainWindow)
+
+}
