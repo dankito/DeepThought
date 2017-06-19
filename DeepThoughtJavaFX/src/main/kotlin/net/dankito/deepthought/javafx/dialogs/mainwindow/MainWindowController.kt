61980d9d7c4242605bbf67a99b4bca9c9f25483b
dankl <dankl@comfylight.com>
2017-06-19 02:22:53 +0200

    Showing ArticleSummaryExtractors from ArticleSummaryExtractorConfigManager

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/MainWindowController.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/MainWindowController.kt
@@ -1,15 +1,36 @@
 package net.dankito.deepthought.javafx.dialogs.mainwindow
 
+import net.dankito.deepthought.javafx.di.AppComponent
+import net.dankito.deepthought.javafx.util.FXUtils
+import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfig
+import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfigManager
+import net.dankito.deepthought.news.summary.config.ConfigChangedListener
 import tornadofx.*
+import javax.inject.Inject
 
 
 class MainWindowController : Controller() {
+
+
+    @Inject
+    protected lateinit var extractorsConfigManager: ArticleSummaryExtractorConfigManager
+
 
     val mainWindow: MainWindow by inject()
 
 
     fun init() {
+        AppComponent.component.inject(this)
 
+        extractorsConfigManager.addListener(articleSummaryExtractorConfigChangedListener)
+        extractorsConfigManager.getConfigs().forEach { mainWindow.addArticleSummaryExtractor(it) }
+    }
+
+
+    private val articleSummaryExtractorConfigChangedListener = object : ConfigChangedListener {
+        override fun configChanged(config: ArticleSummaryExtractorConfig) {
+            FXUtils.runOnUiThread { mainWindow.articleSummaryExtractorUpdated(config) }
+        }
     }
 
 }
