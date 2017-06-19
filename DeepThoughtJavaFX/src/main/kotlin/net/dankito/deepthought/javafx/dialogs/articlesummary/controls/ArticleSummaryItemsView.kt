dae2e62945280338afebaa9e7d4ee817d367913f
dankl <dankl@comfylight.com>
2017-06-19 02:19:49 +0200

    Implemented ArticleSummaryItemsView

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/articlesummary/controls/ArticleSummaryItemsView.kt
@@ -0,0 +1,47 @@
+package net.dankito.deepthought.javafx.dialogs.articlesummary.controls
+
+import javafx.scene.layout.Priority
+import net.dankito.deepthought.javafx.dialogs.articlesummary.ArticleSummaryController
+import net.dankito.newsreader.model.ArticleSummaryItem
+import tornadofx.*
+
+
+class ArticleSummaryItemsView(private val controller: ArticleSummaryController) : View() {
+
+    override val root = listview<ArticleSummaryItem> {
+        items = controller.items
+
+        bindSelected(controller.itemModel)
+
+        prefWidth = 800.0
+        prefHeight = 400.0
+
+        cellFormat {
+            graphic = cache {
+
+                hbox {
+                    imageview(it.previewImageUrl) {
+                        isPreserveRatio = true
+                        fitHeight = 100.0
+                        fitWidth = 120.0
+                    }
+
+                    vbox {
+                        hboxConstraints {
+                            hgrow = Priority.ALWAYS
+                            marginLeftRight(6.0)
+                        }
+
+                        label(it.title)
+
+                        label(it.summary) {
+                            vgrow = Priority.ALWAYS
+
+                            isWrapText = true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
