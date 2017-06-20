9b30335f009e7ac8fbf5c0ffb66acc4b16d01f67
dankl <dankl@comfylight.com>
2017-06-21 01:17:37 +0200

    Created MainActivitySectionsPagerAdapter

---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/MainActivitySectionsPagerAdapter.kt
@@ -0,0 +1,33 @@
+package net.dankito.deepthought.android.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import net.dankito.deepthought.android.fragments.EntriesListView
+import net.dankito.deepthought.android.fragments.TagsListView
+import net.dankito.deepthought.ui.IRouter
+import net.dankito.service.search.ISearchEngine
+
+
+class MainActivitySectionsPagerAdapter(fragmentManager: FragmentManager, searchEngine: ISearchEngine, router: IRouter)
+    : FragmentPagerAdapter(fragmentManager) {
+
+
+    private val entriesListView = EntriesListView(searchEngine, router)
+
+    private val tagsListView = TagsListView()
+
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> return entriesListView
+            1 -> return tagsListView
+            else -> return entriesListView // fallback, but should never happen
+        }
+    }
+
+}
