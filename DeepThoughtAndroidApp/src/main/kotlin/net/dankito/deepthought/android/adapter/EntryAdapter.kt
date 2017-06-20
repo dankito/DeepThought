a647bc21be09572981891d9e3ae0d717cb2835cd
dankl <dankl@comfylight.com>
2017-06-20 15:50:25 +0200

    Using Extension methods now

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/EntryAdapter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/EntryAdapter.kt
@@ -5,17 +5,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.list_item_entry.view.*
 import net.dankito.deepthought.android.R
+import net.dankito.deepthought.extensions.entryPreview
+import net.dankito.deepthought.extensions.referencePreview
 import net.dankito.deepthought.model.Entry
-import net.dankito.deepthought.model.Reference
-import org.jsoup.Jsoup
-import java.text.DateFormat
 
 
 class EntryAdapter: ListAdapter<Entry>() {
-
-    companion object {
-        private val PublishingDateFormat = DateFormat.getDateInstance(DateFormat.SHORT)
-    }
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
@@ -24,46 +19,14 @@ class EntryAdapter: ListAdapter<Entry>() {
         val entry = getItem(position)
 
 
-        view.txtReferencePreview.visibility = View.GONE
-        entry.reference?.let {
-            view.txtReferencePreview.visibility = View.VISIBLE
-            view.txtReferencePreview.text = getReferencePreview(it)
-        }
+        view.txtReferencePreview.visibility = if(entry.hasReference()) View.VISIBLE else View.GONE
+        view.txtReferencePreview.text = entry.referencePreview
 
-        view.txtEntryPreview.text = getEntryPreview(entry) // TODO: externalize to PreviewService
+        view.txtEntryPreview.text = entry.entryPreview
 
         view.txtEntryTags.visibility = if(entry.hasTags()) View.VISIBLE else View.GONE
 
         return view
-    }
-
-
-    private fun getEntryPreview(entry: Entry): String? {
-        var preview = Jsoup.parseBodyFragment(entry.abstractString).text()
-
-        if (preview.length < 200) {
-            if (preview.length > 0) {
-                preview += " "
-            }
-
-            preview += Jsoup.parseBodyFragment(entry.content).text()
-        }
-
-        return preview
-    }
-
-    private fun getReferencePreview(reference: Reference): String? {
-        var preview = reference.title
-
-        var publisherAndDate = reference.series ?: ""
-
-        reference.publishingDate?.let { publisherAndDate += " " + PublishingDateFormat.format(it) }
-
-        if(publisherAndDate.isNullOrBlank() == false) {
-            preview = publisherAndDate.trim() + " - " + preview
-        }
-
-        return preview
     }
 
 }
--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/model/EntryModel.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/model/EntryModel.kt
@@ -2,6 +2,8 @@ package net.dankito.deepthought.javafx.dialogs.mainwindow.model
 
 import javafx.beans.property.SimpleLongProperty
 import javafx.beans.property.SimpleStringProperty
+import net.dankito.deepthought.extensions.entryPreview
+import net.dankito.deepthought.extensions.referencePreview
 import net.dankito.deepthought.model.Entry
 import tornadofx.*
 
@@ -10,8 +12,12 @@ class EntryModel : ItemViewModel<Entry>() {
 
     val index = bind { SimpleLongProperty(item?.entryIndex ?: 0) }
 
-    val reference = bind { SimpleStringProperty(item?.reference?.toString() ?: "") }
+    val reference = bind { SimpleStringProperty(item?.referencePreview ?: "") }
 
-    val preview = bind { SimpleStringProperty(item?.content ?: "") }
+    val preview = bind { SimpleStringProperty(item?.entryPreview ?: "") }
+
+    val createdOn = bind { SimpleLongProperty(item?.createdOn?.time ?: 0) }
+
+    val modifiedOn = bind { SimpleLongProperty(item?.modifiedOn?.time ?: 0) }
 
 }
