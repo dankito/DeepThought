b46a93bd25ee91ca1bcc003ae319659af8a36050
dankl <dankl@comfylight.com>
2017-06-21 10:35:11 +0200

    Added property tags

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/EntryExtractionResult.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/EntryExtractionResult.kt
@@ -2,9 +2,11 @@ package net.dankito.newsreader.model
 
 import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Reference
+import net.dankito.deepthought.model.Tag
 
 
-data class EntryExtractionResult(val entry: Entry, val reference: Reference? = null) {
+data class EntryExtractionResult(val entry: Entry, val reference: Reference? = null, val tags: MutableList<Tag> = ArrayList()) {
+
 
     private constructor() : this(Entry("")) // for Jackson
 }
