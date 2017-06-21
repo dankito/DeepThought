77e890918a78aef0671cfc61cf09963af396afb9
dankl <dankl@comfylight.com>
2017-06-21 16:45:05 +0200

    Added search functionality to EntriesListView

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/EntriesListView.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/EntriesListView.kt
@@ -1,11 +1,11 @@
 package net.dankito.deepthought.android.fragments
 
+import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.SearchView
+import android.view.*
 import kotlinx.android.synthetic.main.fragment_tab_entries.view.*
 import net.dankito.deepthought.android.R
 import net.dankito.deepthought.android.adapter.EntryAdapter
@@ -34,6 +34,8 @@ class EntriesListView(private val searchEngine: ISearchEngine, private val route
     private fun setupUI(rootView: View?) {
         rootView?.lstEntries?.adapter = entryAdapter
         rootView?.lstEntries?.setOnItemClickListener { _, _, position, _ -> presenter.showEntry(entryAdapter.getItem(position)) }
+
+        setHasOptionsMenu(true)
     }
 
     override fun onAttach(context: Context?) {
@@ -46,6 +48,36 @@ class EntriesListView(private val searchEngine: ISearchEngine, private val route
         presenter.cleanUp()
 
         super.onDestroy()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.fragment_tab_entries_menu, menu)
+
+        // TODO: this is almost the same code as in TagsFragment -> merge
+        // Associate searchable configuration with the SearchView
+        val searchItem = menu.findItem(R.id.search)
+
+        (searchItem?.actionView as? SearchView)?.let { searchView ->
+            val searchManager = activity.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(activity.componentName))
+            searchView.queryHint = activity.getString(R.string.search_hint_entries)
+            searchView.setOnQueryTextListener(entriesQueryTextListener)
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+    private val entriesQueryTextListener: SearchView.OnQueryTextListener = object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String): Boolean {
+            return onQueryTextChange(query)
+        }
+
+        override fun onQueryTextChange(query: String): Boolean {
+            presenter.searchEntries(query)
+            return true
+        }
     }
 
 
