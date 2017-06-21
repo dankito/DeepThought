ee264a0a3cc25fd636633d188af02e98708a80f7
dankl <dankl@comfylight.com>
2017-06-21 11:24:08 +0200

    Added TagService

---
+++ libs/service_layer/data/DataServices/src/main/kotlin/net/dankito/service/data/TagService.kt
@@ -0,0 +1,27 @@
+package net.dankito.service.data
+
+import net.dankito.deepthought.model.BaseEntity
+import net.dankito.deepthought.model.DeepThought
+import net.dankito.deepthought.model.Tag
+import net.dankito.deepthought.service.data.DataManager
+import net.dankito.service.data.messages.EntityChangeType
+import net.dankito.service.data.messages.EntityChanged
+import net.dankito.service.data.messages.TagChanged
+import net.dankito.service.eventbus.IEventBus
+
+
+class TagService(dataManager: DataManager, eventBus: IEventBus) : EntityServiceBase<Tag>(dataManager, eventBus) {
+
+    override fun getEntityClass(): Class<Tag> {
+        return Tag::class.java
+    }
+
+    override fun addEntityToDeepThought(deepThought: DeepThought, entity: Tag) {
+        deepThought.addTag(entity)
+    }
+
+    override fun createEntityChangedMessage(entity: Tag, changeType: EntityChangeType): EntityChanged<out BaseEntity> {
+        return TagChanged(entity, changeType)
+    }
+
+}
