3d5c71bf4838ef3ce5bf048361f3a20894207c7c
dankl <dankl@comfylight.com>
2017-06-21 10:40:03 +0200

    Also using EntryPersister in ViewEntryPresenter

--- DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/ViewEntryPresenter.kt
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/ViewEntryPresenter.kt
@@ -1,36 +1,25 @@
 package net.dankito.deepthought.ui.presenter
 
 import net.dankito.deepthought.model.Entry
-import net.dankito.deepthought.model.Reference
 import net.dankito.deepthought.ui.IRouter
+import net.dankito.deepthought.ui.presenter.util.EntryPersister
 import net.dankito.newsreader.model.EntryExtractionResult
 import net.dankito.service.data.EntryService
 import net.dankito.service.data.ReferenceService
 
 
 class ViewEntryPresenter(private var entry: Entry?, private var entryExtractionResult: EntryExtractionResult?,
-                         private var entryService: EntryService, private var referenceService: ReferenceService, private var router: IRouter) {
+                         entryService: EntryService, referenceService: ReferenceService, private var router: IRouter) {
+
+    private val entryPersister = EntryPersister(entryService, referenceService)
+
 
     fun saveEntry() {
-        entryExtractionResult?.let { entryExtractionResult ->
-            val entry = entryExtractionResult.entry
-
-            entry.reference = createAndPersistReference(entryExtractionResult)
-
-            entryService.persist(entry)
+        entryExtractionResult?.let { result ->
+            entryPersister.saveEntry(result.entry, result.reference, result.tags)
         }
 
         returnToPreviousView()
-    }
-
-    private fun createAndPersistReference(entryExtractionResult: EntryExtractionResult): Reference? {
-        val reference = entryExtractionResult.reference
-
-        if(reference != null) {
-            referenceService.persist(reference)
-        }
-
-        return reference
     }
 
 
