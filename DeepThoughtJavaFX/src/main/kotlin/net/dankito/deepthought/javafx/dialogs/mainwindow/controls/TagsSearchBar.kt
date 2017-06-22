9c51dd02b2893f437cd9cb295d43e98717ecffa4
dankl <dankl@comfylight.com>
2017-06-22 10:54:56 +0200

    Implemented TagsSearchBar

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/controls/TagsSearchBar.kt
@@ -0,0 +1,36 @@
+package net.dankito.deepthought.javafx.dialogs.mainwindow.controls
+
+import javafx.geometry.Pos
+import javafx.scene.input.KeyCode
+import javafx.scene.layout.Priority
+import net.dankito.deepthought.ui.presenter.TagsListPresenter
+import tornadofx.*
+
+
+class TagsSearchBar(private val presenter: TagsListPresenter) : View() {
+
+    override val root = hbox {
+        prefHeight = 40.0
+        useMaxWidth = true
+        alignment = Pos.CENTER_LEFT
+
+        label("Search:")
+
+        textfield {
+            hboxConstraints {
+                hgrow = Priority.ALWAYS
+                marginLeftRight(6.0)
+            }
+
+            promptText = "TODO"
+
+            textProperty().addListener { _, _, newValue -> presenter.searchTags(newValue) }
+            setOnKeyReleased { event ->
+                if(event.code == KeyCode.ESCAPE) {
+                    clear()
+                }
+            }
+        }
+    }
+
+}
