9d9a4aebc10e6ebcefbbc44b5e56db5461f11809
dankl <dankl@comfylight.com>
2017-06-22 19:32:07 +0200

    Further merged constructors

--- libs/service_layer/search/SearchEngine/src/main/kotlin/net/dankito/service/search/specific/EntriesSearch.kt
+++ libs/service_layer/search/SearchEngine/src/main/kotlin/net/dankito/service/search/specific/EntriesSearch.kt
@@ -5,22 +5,11 @@ import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Tag
 import net.dankito.service.search.Search
 import net.dankito.service.search.SearchWithCollectionResult
-import java.util.*
 
 
 class EntriesSearch(searchTerm: String = Search.EmptySearchTerm, val filterContent: Boolean = true, val filterAbstract: Boolean = true,
+                    val filterOnlyEntriesWithoutTags: Boolean = false, val entriesMustHaveTheseTags: MutableCollection<Tag> = mutableListOf(),
                     completedListener: (List<Entry>) -> Unit) : SearchWithCollectionResult<Entry>(searchTerm, completedListener) {
-
-    var filterOnlyEntriesWithoutTags: Boolean = false
-
-    var entriesMustHaveTheseTags: MutableCollection<Tag> = ArrayList()
-
-
-    constructor(searchTerm: String, filterContent: Boolean, filterAbstract: Boolean, filterOnlyEntriesWithoutTags: Boolean, entriesMustHaveTheseTags: MutableCollection<Tag>, completedListener: (List<Entry>) -> Unit)
-            : this(searchTerm, filterContent, filterAbstract, completedListener) {
-        this.filterOnlyEntriesWithoutTags = filterOnlyEntriesWithoutTags
-        this.entriesMustHaveTheseTags = entriesMustHaveTheseTags
-    }
 
 
     fun addTagEntriesMustHave(tag: Tag): Boolean {
