83f77e57a4c18b7264d9c7fa1e050a9f7077ca8c
dankl <dankl@comfylight.com>
2017-06-23 18:07:12 +0200

    Added convenience methods for extractArticlesSummary() and loadMoreItems()

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/articlesummary/presenter/JavaFXArticleSummaryPresenter.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/articlesummary/presenter/JavaFXArticleSummaryPresenter.kt
@@ -1,5 +1,6 @@
 package net.dankito.deepthought.javafx.dialogs.articlesummary.presenter
 
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.collections.FXCollections
 import net.dankito.deepthought.javafx.dialogs.articlesummary.model.ArticleSummaryItemModel
 import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfig
@@ -7,6 +8,7 @@ import net.dankito.deepthought.ui.IRouter
 import net.dankito.deepthought.ui.presenter.ArticleSummaryPresenter
 import net.dankito.deepthought.ui.presenter.util.EntryPersister
 import net.dankito.newsreader.article.ArticleExtractors
+import net.dankito.newsreader.model.ArticleSummary
 import net.dankito.newsreader.model.ArticleSummaryItem
 import net.dankito.newsreader.model.EntryExtractionResult
 import net.dankito.service.data.ReadLaterArticleService
@@ -18,7 +20,7 @@ import tornadofx.*
 /**
  * Yeah, i know this is bad design, a Controller deriving from a Presenter ...
  */
-class JavaFXArticleSummaryPresenter(articleSummaryExtractor: ArticleSummaryExtractorConfig, articleExtractors: ArticleExtractors, entryPersister: EntryPersister,
+class JavaFXArticleSummaryPresenter(private val articleSummaryExtractor: ArticleSummaryExtractorConfig, articleExtractors: ArticleExtractors, entryPersister: EntryPersister,
                                     readLaterArticleService: ReadLaterArticleService, tagService: TagService, searchEngine: ISearchEngine, router: IRouter)
     : ArticleSummaryPresenter(articleExtractors, entryPersister, readLaterArticleService, tagService, searchEngine, router) {
 
@@ -26,12 +28,41 @@ class JavaFXArticleSummaryPresenter(articleSummaryExtractor: ArticleSummaryExtra
 
     val items = FXCollections.observableArrayList<ArticleSummaryItem>()
 
+    val canLoadMoreItems = SimpleBooleanProperty(false)
+
 
     init {
+        extractArticlesSummary()
+    }
+
+
+    fun extractArticlesSummary() {
         extractArticlesSummary(articleSummaryExtractor) {
-            it.result?.let { items.setAll(it.articles) }
+            it.result?.let { articleSummaryReceived(it, false) }
+            // TODO: show error elsewise
         }
     }
+
+    fun loadMoreItems() {
+        loadMoreItems(articleSummaryExtractor) {
+            it.result?.let { articleSummaryReceived(it, true) }
+            // TODO: show error elsewise
+        }
+    }
+
+    private fun articleSummaryReceived(articleSummary: ArticleSummary, hasLoadedMoreItems: Boolean) {
+        runLater {
+            if(hasLoadedMoreItems) {
+                items.addAll(articleSummary.articles)
+            }
+            else {
+                items.setAll(articleSummary.articles)
+            }
+
+            canLoadMoreItems.set(articleSummary.canLoadMoreItems)
+        }
+    }
+
 
     fun getAndShowArticle(item: ArticleSummaryItem) {
         getAndShowArticle(item) {
