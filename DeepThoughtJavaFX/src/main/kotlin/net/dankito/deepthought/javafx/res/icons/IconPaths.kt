329b3db6d21049764d800f6409cfcae1ca3a9580
dankl <dankl@comfylight.com>
2017-06-23 17:58:49 +0200

    Added icons

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/res/icons/IconPaths.kt
@@ -0,0 +1,20 @@
+package net.dankito.deepthought.javafx.res.icons
+
+
+class IconPaths {
+
+    companion object {
+
+        const val FilterIconPath = "icons/filter_22x22.png"
+
+        const val FilterDeleteIconPath = "icons/filter_delete_20x20.png"
+
+        const val NewspaperIconPath = "icons/news_icon_26x26.png"
+
+        const val UpdateIconPath = "icons/update_icon_32x32.png"
+
+        const val LoadNextItemsIconPath = "icons/arrow_right_in_circle_32x32.png"
+
+    }
+
+}
