bdce2829fc9ee1bd0f7989549672d9ed6f846876
dankl <dankl@comfylight.com>
2017-06-23 20:55:05 +0200

    Implemented ReadLaterArticleListCellFragment

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/readlaterarticle/controls/ReadLaterArticleListCellFragment.kt
@@ -0,0 +1,59 @@
+package net.dankito.deepthought.javafx.dialogs.articlesummary.controls
+
+import javafx.geometry.Pos
+import javafx.scene.layout.Priority
+import javafx.scene.layout.Region
+import net.dankito.deepthought.javafx.dialogs.readlaterarticle.model.ReadLaterArticleViewModel
+import net.dankito.deepthought.javafx.util.FXUtils
+import net.dankito.newsreader.model.EntryExtractionResult
+import tornadofx.*
+
+
+class ReadLaterArticleListCellFragment : ListCellFragment<EntryExtractionResult>() {
+
+    val article = ReadLaterArticleViewModel().bindTo(this)
+
+
+    override val root = hbox {
+        cellProperty.addListener { _, _, newValue -> // so that the graphic always has cell's width
+            newValue?.let { prefWidthProperty().bind(it.widthProperty().subtract(16)) }
+        }
+
+        alignment = Pos.CENTER
+        minHeight = 100.0
+        prefHeight = 100.0
+
+        imageview(article.previewImageUrl) {
+            isPreserveRatio = true
+            fitHeight = 100.0
+            fitWidth = 120.0
+        }
+
+        vbox {
+            useMaxHeight = true
+            prefHeight = Region.USE_COMPUTED_SIZE
+            alignment = Pos.CENTER_LEFT
+
+            hboxConstraints {
+                hgrow = Priority.ALWAYS
+                marginLeftRight(6.0)
+            }
+
+            label(article.reference) {
+                maxHeight = 20.0
+                FXUtils.ensureNodeOnlyUsesSpaceIfVisible(this)
+
+                vboxConstraints {
+                    marginBottom = 6.0
+                }
+            }
+
+            label(article.summary) {
+                vgrow = Priority.ALWAYS
+
+                isWrapText = true
+            }
+        }
+    }
+
+}
