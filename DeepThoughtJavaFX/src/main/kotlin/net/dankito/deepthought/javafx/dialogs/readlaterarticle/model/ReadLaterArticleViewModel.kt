33312c3c82981a9035ad1c064371929dc2e0ac8f
dankl <dankl@comfylight.com>
2017-06-23 20:54:43 +0200

    Added ReadLaterArticleViewModel

---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/readlaterarticle/model/ReadLaterArticleViewModel.kt
@@ -0,0 +1,18 @@
+package net.dankito.deepthought.javafx.dialogs.readlaterarticle.model
+
+import javafx.beans.property.SimpleStringProperty
+import net.dankito.deepthought.extensions.entryPreview
+import net.dankito.deepthought.extensions.preview
+import net.dankito.newsreader.model.EntryExtractionResult
+import tornadofx.*
+
+
+class ReadLaterArticleViewModel : ItemViewModel<EntryExtractionResult>() {
+
+    val previewImageUrl = bind { SimpleStringProperty(item?.entry?.previewImageUrl) }
+
+    val reference = bind { SimpleStringProperty(item?.reference?.preview ?: "") }
+
+    val summary = bind { SimpleStringProperty(item?.entry?.entryPreview ?: "") }
+
+}
