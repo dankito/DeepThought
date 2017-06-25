6c5f6af5676f09768a0796bc5cd7010a01c0098a
dankl <dankl@comfylight.com>
2017-06-26 00:20:06 +0200

    Added default values

--- DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/IDialogService.kt
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/IDialogService.kt
@@ -3,12 +3,12 @@ package net.dankito.deepthought.ui
 
 interface IDialogService {
 
-    fun showInfoMessage(infoMessage: String, alertTitle: String?)
+    fun showInfoMessage(infoMessage: String, alertTitle: String? = null)
 
-    fun showConfirmationDialog(message: String, alertTitle: String?, optionSelected: (Boolean) -> Unit)
+    fun showConfirmationDialog(message: String, alertTitle: String? = null, optionSelected: (Boolean) -> Unit)
 
-    fun showErrorMessage(errorMessage: String, alertTitle: String?, exception: Exception?)
+    fun showErrorMessage(errorMessage: String, alertTitle: String? = null, exception: Exception? = null)
 
-    fun askForTextInput(questionText: String, alertTitleText: String?, defaultValue: String?, callback: (Boolean, String?) -> Unit)
+    fun askForTextInput(questionText: String, alertTitleText: String? = null, defaultValue: String? = null, callback: (Boolean, String?) -> Unit)
 
 }
