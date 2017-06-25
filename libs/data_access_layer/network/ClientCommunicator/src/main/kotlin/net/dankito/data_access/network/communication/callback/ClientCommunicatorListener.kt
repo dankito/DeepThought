807783362b37678f3a6d8190c30995b905fd311a
dankl <dankl@comfylight.com>
2017-06-25 21:19:42 +0200

    Removed now unused ClientCommunicatorListener

--- libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/callback/ClientCommunicatorListener.kt
+++
@@ -1,8 +0,0 @@
-package net.dankito.data_access.network.communication.callback
-
-
-interface ClientCommunicatorListener {
-
-    fun started(couldStartMessagesReceiver: Boolean, messagesReceiverPort: Int, startException: Exception?)
-
-}
