44d25b6d4860e42f716f515aa175c0c8ac44a344
dankl <dankl@comfylight.com>
2017-06-25 21:53:13 +0200

    Removed unused import

--- libs/data_access_layer/network/TcpSocketClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/RequestSender.kt
+++ libs/data_access_layer/network/TcpSocketClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/RequestSender.kt
@@ -1,6 +1,5 @@
 package net.dankito.data_access.network.communication
 
-import net.dankito.data_access.network.communication.callback.SendRequestCallback
 import net.dankito.data_access.network.communication.message.Request
 import net.dankito.data_access.network.communication.message.Response
 import net.dankito.data_access.network.communication.message.ResponseErrorType
