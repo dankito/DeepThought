3cde7a240002a52a1b6b67063e23901134852962
dankl <dankl@comfylight.com>
2017-06-26 00:35:12 +0200

    Added CommunicationManagerStarter

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/di/JavaFXModule.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/di/JavaFXModule.kt
@@ -3,17 +3,26 @@ package net.dankito.deepthought.javafx.di
 import dagger.Module
 import dagger.Provides
 import net.dankito.data_access.network.communication.callback.IsSynchronizationPermittedHandler
+import net.dankito.deepthought.android.appstart.CommunicationManagerStarter
 import net.dankito.deepthought.javafx.dialogs.JavaFXDialogService
 import net.dankito.deepthought.javafx.dialogs.mainwindow.MainWindowController
 import net.dankito.deepthought.javafx.routing.JavaFXRouter
 import net.dankito.deepthought.javafx.service.communication.JavaFXIsSynchronizationPermittedHandler
 import net.dankito.deepthought.ui.IDialogService
 import net.dankito.deepthought.ui.IRouter
+import net.dankito.service.search.ISearchEngine
 import javax.inject.Singleton
 
 
 @Module
 class JavaFXModule(private val mainWindowController: MainWindowController) {
+
+    @Provides
+    @Singleton
+    fun provideCommunicationManagerStarter(searchEngine: ISearchEngine) : CommunicationManagerStarter {
+        return CommunicationManagerStarter(searchEngine)
+    }
+
 
     @Provides
     @Singleton
