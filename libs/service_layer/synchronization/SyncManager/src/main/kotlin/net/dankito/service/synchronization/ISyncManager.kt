41dc2d7d5df327fb1e794e8705a2294c86df1bdb
dankl <dankl@comfylight.com>
2017-06-25 19:35:59 +0200

    Added ISyncManager

---
+++ libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/ISyncManager.kt
@@ -0,0 +1,22 @@
+package net.dankito.service.synchronization
+
+import net.dankito.deepthought.model.Device
+
+
+interface ISyncManager {
+
+    companion object {
+        const val DefaultBasicDataSynchronizationListenerPort = 32790
+
+        const val DefaultSynchronizationListenerPort = 32791
+
+        const val DefaultAlsoUsePullReplication = false
+    }
+
+
+    fun startAsync(desiredSynchronizationPort: Int = DefaultSynchronizationListenerPort, desiredBasicDataSynchronizationPort: Int = DefaultBasicDataSynchronizationListenerPort,
+                   alsoUsePullReplication: Boolean = DefaultAlsoUsePullReplication, initializedCallback: (Int) -> Unit)
+
+    fun syncBasicDataWithDevice(deviceId: String, remoteDeviceAddress: String, basicDataSyncPort: Int, syncDone: (Device) -> Unit)
+
+}
