747dbf32b7cdf1109339d6ce7653f97cec325688
dankl <dankl@comfylight.com>
2017-06-27 15:25:59 +0200

    Also sorting applicationLanguages by sortOrder

--- libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/DeepThought.kt
+++ libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/DeepThought.kt
@@ -40,7 +40,7 @@ data class DeepThought(
 
     @OneToMany(fetch = FetchType.EAGER, cascade = arrayOf(CascadeType.PERSIST))
     @OrderBy(value = "sortOrder")
-    var applicationLanguages: MutableSet<ApplicationLanguage> = HashSet<ApplicationLanguage>() // these are the Languages the UI can display
+    var applicationLanguages: MutableSet<ApplicationLanguage> = TreeSet<ApplicationLanguage>() // these are the Languages the UI can display
         private set
 
     @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.PERSIST))
