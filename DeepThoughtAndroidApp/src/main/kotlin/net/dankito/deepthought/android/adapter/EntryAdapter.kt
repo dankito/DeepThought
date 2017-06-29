3c538905fc185e25b90698e09e40ab252bcdbbcb
dankl <dankl@comfylight.com>
2017-06-29 21:15:03 +0200

    EntryAdapter now derives from ListSwipeAdapter to show a view when user swipes from left to right

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/EntryAdapter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/EntryAdapter.kt
@@ -4,34 +4,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.daimajia.swipe.SwipeLayout
 import kotlinx.android.synthetic.main.list_item_entry.view.*
 import net.dankito.deepthought.android.R
 import net.dankito.deepthought.extensions.entryPreview
 import net.dankito.deepthought.extensions.referencePreview
 import net.dankito.deepthought.extensions.tagsPreview
 import net.dankito.deepthought.model.Entry
+import net.dankito.deepthought.ui.presenter.EntriesListPresenter
 
 
-class EntryAdapter: ListAdapter<Entry>() {
+class EntryAdapter(private val presenter: EntriesListPresenter): ListSwipeAdapter<Entry>() {
 
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.list_item_entry, parent, false)
+    override fun getSwipeLayoutResourceId(position: Int): Int {
+        return R.id.swipe
+    }
 
+    override fun generateView(position: Int, parent: ViewGroup?): View {
+        return LayoutInflater.from(parent?.context).inflate(R.layout.list_item_entry, parent, false)
+    }
+
+    override fun fillValues(position: Int, convertView: View) {
         val entry = getItem(position)
 
+        convertView.txtReferencePreview.visibility = if(entry.hasReference()) View.VISIBLE else View.GONE
+        convertView.txtReferencePreview.text = entry.referencePreview
 
-        view.txtReferencePreview.visibility = if(entry.hasReference()) View.VISIBLE else View.GONE
-        view.txtReferencePreview.text = entry.referencePreview
+        convertView.txtEntryPreview.text = entry.entryPreview
+        setTxtEntryPreviewMaxLines(convertView.txtEntryPreview, entry)
 
-        view.txtEntryPreview.text = entry.entryPreview
-        setTxtEntryPreviewMaxLines(view.txtEntryPreview, entry)
+        convertView.txtEntryTags.visibility = if(entry.hasTags()) View.VISIBLE else View.GONE
+        convertView.txtEntryTags.text = entry.tagsPreview
 
-        view.txtEntryTags.visibility = if(entry.hasTags()) View.VISIBLE else View.GONE
-        view.txtEntryTags.text = entry.tagsPreview
+        convertView.btnShareEntry.setOnClickListener { presenter.shareReferenceUrl(entry) }
 
-        return view
+        convertView.btnDeleteEntry.setOnClickListener {
+            presenter.deleteEntry(entry)
+            (convertView as? SwipeLayout)?.close()
+        }
     }
+
 
     private fun setTxtEntryPreviewMaxLines(txtEntryPreview: TextView, entry: Entry) {
         var countPreviewLines = if(entry.hasReference()) 3 else 4
