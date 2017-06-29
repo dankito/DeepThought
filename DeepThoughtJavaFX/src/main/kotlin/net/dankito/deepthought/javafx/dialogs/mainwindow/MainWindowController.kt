66dfa58ab45a4dcc7887126701c6c77db62691b6
dankl <dankl@comfylight.com>
2017-06-29 22:08:43 +0200

    Removed unused imports

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/MainWindowController.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/MainWindowController.kt
@@ -4,14 +4,12 @@ import net.dankito.deepthought.javafx.di.AppComponent
 import net.dankito.deepthought.javafx.dialogs.mainwindow.model.EntryViewModel
 import net.dankito.deepthought.javafx.util.FXUtils
 import net.dankito.deepthought.javafx.util.LazyLoadingObservableList
-import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.ArticleSummaryExtractorConfig
+import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.news.summary.config.ArticleSummaryExtractorConfigManager
-import net.dankito.deepthought.news.summary.config.ConfigChangedListener
 import net.dankito.deepthought.ui.IRouter
 import net.dankito.service.data.EntryService
 import net.dankito.service.data.messages.ArticleSummaryExtractorConfigChanged
-import net.dankito.service.data.messages.EntitiesOfTypeChanged
 import net.dankito.service.eventbus.IEventBus
 import net.engio.mbassy.listener.Handler
 import tornadofx.*
