cf0fbdc3a2e4850cb5c330cf409f03365668b686
dankl <dankl@comfylight.com>
2017-07-04 15:11:59 +0200

    Made no-arg constructor public

--- libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/initialsync/model/SyncInfo.kt
+++ libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/initialsync/model/SyncInfo.kt
@@ -3,5 +3,5 @@ package net.dankito.service.synchronization.initialsync.model
 
 data class SyncInfo(val deepThought: DeepThoughtSyncInfo, val user: UserSyncInfo, val useCallerDatabaseIds: Boolean? = null, val useCallerUserName: Boolean? = null) {
 
-    private constructor() : this(DeepThoughtSyncInfo(), UserSyncInfo()) // for Jackson
+    constructor() : this(DeepThoughtSyncInfo(), UserSyncInfo()) // for Jackson
 }
