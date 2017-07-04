d3aa13e471c88c6d2edd9b5afc6390241e2c619e
dankl <dankl@comfylight.com>
2017-07-04 18:43:50 +0200

    Added convenience constructor with User as parameter

--- libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/initialsync/model/UserSyncInfo.kt
+++ libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/initialsync/model/UserSyncInfo.kt
@@ -1,8 +1,13 @@
 package net.dankito.service.synchronization.initialsync.model
 
+import net.dankito.deepthought.model.User
+
 
 open class UserSyncInfo(val id: String, val userName: String, val universallyUniqueId: String, val firstName: String, val lastName: String,
                         val countSynchronizedDevices: Int, val countIgnoredDevices: Int) {
 
     internal constructor() : this("", "", "", "", "", 0, 0) // for Jackson
+
+    constructor(user: User) : this(user.id!!, user.userName, user.universallyUniqueId, user.firstName, user.lastName, user.synchronizedDevices.size, user.ignoredDevices.size)
+
 }
