a6ba4945c79545aca2f2e62d7d9d432c61dcc1c0
dankl <dankl@comfylight.com>
2017-07-05 09:43:55 +0200

    Added remoteRequestedToStartSynchronizationListeners

--- libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/MessageHandlerConfig.kt
+++ libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/MessageHandlerConfig.kt
@@ -1,7 +1,21 @@
 package net.dankito.data_access.network.communication.message
 
 import net.dankito.data_access.network.communication.callback.IDeviceRegistrationHandler
+import net.dankito.deepthought.model.DiscoveredDevice
 import net.dankito.deepthought.model.INetworkSettings
 
 
-data class MessageHandlerConfig(val networkSettings: INetworkSettings, val challengeHandler: ChallengeHandler, val registrationHandler: IDeviceRegistrationHandler)
+data class MessageHandlerConfig(val networkSettings: INetworkSettings, val challengeHandler: ChallengeHandler, val registrationHandler: IDeviceRegistrationHandler) {
+
+    private val remoteRequestedToStartSynchronizationListeners = HashSet<(DiscoveredDevice) -> Unit>()
+
+
+    fun addRemoteRequestedToStartSynchronizationListener(listener: (DiscoveredDevice) -> Unit) {
+        remoteRequestedToStartSynchronizationListeners.add(listener)
+    }
+
+    fun callRemoteRequestedToStartSynchronizationListeners(device: DiscoveredDevice) {
+        remoteRequestedToStartSynchronizationListeners.forEach { it(device) }
+    }
+
+}
