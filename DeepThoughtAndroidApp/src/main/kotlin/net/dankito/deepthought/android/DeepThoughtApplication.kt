603c693d549a86235301f57b548044d32f4f69ec
dankl <dankl@comfylight.com>
2017-07-11 00:03:37 +0200

    Using now DeepThoughtBackgroundAndroidService to initialize application

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/DeepThoughtApplication.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/DeepThoughtApplication.kt
@@ -1,37 +1,40 @@
 package net.dankito.deepthought.android
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
 import android.support.multidex.MultiDexApplication
-import net.dankito.deepthought.android.appstart.AndroidAppInitializer
-import net.dankito.deepthought.android.di.ActivitiesModule
-import net.dankito.deepthought.android.di.AppComponent
-import net.dankito.deepthought.android.di.DaggerAppComponent
-import net.dankito.deepthought.di.BaseComponent
-import net.dankito.deepthought.di.CommonComponent
-import net.dankito.deepthought.di.CommonModule
-import javax.inject.Inject
+import net.dankito.deepthought.android.androidservice.DeepThoughtBackgroundAndroidService
+import net.dankito.deepthought.android.androidservice.DeepThoughtBackgroundAndroidServiceBinder
 
 
 class DeepThoughtApplication : MultiDexApplication() {
 
-    @Inject
-    protected lateinit var appInitializer: AndroidAppInitializer
+    private var service: DeepThoughtBackgroundAndroidService? = null
 
 
     override fun onCreate() {
         super.onCreate()
 
-        val component = DaggerAppComponent.builder()
-                .commonModule(CommonModule())
-                .activitiesModule(ActivitiesModule(this))
-                .build()
+        val intent = Intent(this, DeepThoughtBackgroundAndroidService::class.java)
+        startService(intent)
+        bindService(intent, deepThoughtBackgroundServiceConnection, Context.BIND_AUTO_CREATE)
+    }
 
-        BaseComponent.component = component
-        CommonComponent.component = component
-        AppComponent.component = component
 
-        component.inject(this)
+    private val deepThoughtBackgroundServiceConnection: ServiceConnection = object : ServiceConnection {
 
-        appInitializer.initializeApp()
+        override fun onServiceConnected(className: ComponentName, binder: IBinder) {
+            val backgroundServiceBinder = binder as DeepThoughtBackgroundAndroidServiceBinder
+            service = backgroundServiceBinder.service
+        }
+
+        override fun onServiceDisconnected(className: ComponentName) {
+            service = null
+        }
+
     }
 
 }
