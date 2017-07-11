17783529129ed4ff2742fd25e19751154eb5b226
dankl <dankl@comfylight.com>
2017-07-11 14:42:16 +0200

    Added IReferencesListView

---
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/view/IReferencesListView.kt
@@ -0,0 +1,10 @@
+package net.dankito.deepthought.ui.view
+
+import net.dankito.deepthought.model.Reference
+
+
+interface IReferencesListView {
+
+    fun showReferences(references: List<Reference>)
+
+}
