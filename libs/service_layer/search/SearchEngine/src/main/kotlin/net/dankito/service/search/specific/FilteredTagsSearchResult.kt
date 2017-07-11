e5f836c71ecab125aff0568add9ae8116e082f4a
dankl <dankl@comfylight.com>
2017-07-11 23:38:37 +0200

    Renamed to FilteredTagsSearchResult

--- libs/service_layer/search/SearchEngine/src/main/kotlin/net/dankito/service/search/specific/FilteredTagsSearchResult.kt
+++ libs/service_layer/search/SearchEngine/src/main/kotlin/net/dankito/service/search/specific/FilteredTagsSearchResult.kt
@@ -5,7 +5,7 @@ import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Tag
 
 
-class FindAllEntriesHavingTheseTagsResult(val entriesHavingFilteredTags: List<Entry>, val tagsOnEntriesContainingFilteredTags: List<Tag>) {
+class FilteredTagsSearchResult(val entriesHavingFilteredTags: List<Entry>, val tagsOnEntriesContainingFilteredTags: List<Tag>) {
 
     val tagsOnEntriesContainingFilteredTagsCount: Int
         get() = tagsOnEntriesContainingFilteredTags.size
