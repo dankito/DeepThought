7b8743323dd22f5f8e46a4c32ce589a362b03698
dankl <dankl@comfylight.com>
2017-07-11 14:45:19 +0200

    Added ic_book_white_48dp

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/MainActivitySectionsPagerAdapter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/MainActivitySectionsPagerAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import net.dankito.deepthought.android.fragments.EntriesListView
 import net.dankito.deepthought.android.fragments.MainActivityTabFragment
+import net.dankito.deepthought.android.fragments.ReferencesListView
 import net.dankito.deepthought.android.fragments.TagsListView
 
 
@@ -14,16 +15,19 @@ class MainActivitySectionsPagerAdapter(fragmentManager: FragmentManager) : Fragm
 
     private val tagsListView = TagsListView()
 
+    private val referencesListView = ReferencesListView()
+
 
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 
     override fun getItem(position: Int): MainActivityTabFragment {
         when(position) {
             0 -> return entriesListView
             1 -> return tagsListView
-            else -> return entriesListView // fallback, but should never happen
+            2 -> return referencesListView
+            else -> return entriesListView // to make compiler happy
         }
     }
 
