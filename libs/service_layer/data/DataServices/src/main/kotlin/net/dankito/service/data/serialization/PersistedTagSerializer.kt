60b82fc3a850677bd9fd5350980cb2a4b74cab9a
dankl <dankl@comfylight.com>
2017-07-11 18:46:08 +0200

    Added PersistedTagSerializer

---
+++ libs/service_layer/data/DataServices/src/main/kotlin/net/dankito/service/data/serialization/PersistedTagSerializer.kt
@@ -0,0 +1,23 @@
+package net.dankito.service.data.serialization
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import net.dankito.deepthought.model.Tag
+
+
+class PersistedTagSerializer : StdSerializer<Tag>(Tag::class.java) {
+
+    override fun serialize(value: Tag?, gen: JsonGenerator?, provider: SerializerProvider?) {
+        value?.let { value ->
+            gen?.let { jsonGenerator ->
+                jsonGenerator.writeStartObject()
+
+                jsonGenerator.writeStringField(SerializerConfig.TagIdFieldName, value.id)
+
+                jsonGenerator.writeEndObject()
+            }
+        }
+    }
+
+}
