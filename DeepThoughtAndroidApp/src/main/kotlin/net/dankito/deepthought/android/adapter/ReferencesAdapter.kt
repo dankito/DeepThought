5feef3f6946e23efd6a22f564e712c2ce879ceb9
dankl <dankl@comfylight.com>
2017-07-11 14:39:59 +0200

    Implemented ReferencesAdapter

---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/adapter/ReferencesAdapter.kt
@@ -0,0 +1,27 @@
+package net.dankito.deepthought.android.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.list_item_reference.view.*
+import net.dankito.deepthought.android.R
+import net.dankito.deepthought.extensions.preview
+import net.dankito.deepthought.model.Reference
+
+
+class ReferencesAdapter : ListAdapter<Reference>() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(parent?.context).inflate(R.layout.list_item_reference, parent, false)
+
+        val reference = getItem(position)
+
+        view.txtReferencePreview.text = reference.preview
+
+        view.imgEditReference.setOnClickListener { Toast.makeText(view.context, "TODO: Show edit reference scree", Toast.LENGTH_LONG).show() }
+
+        return view
+    }
+
+}
