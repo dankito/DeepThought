80b3bd162efb26530803152d2de51e71ffed1124
dankl <dankl@comfylight.com>
2017-07-11 20:08:07 +0200

    Optionally storing ArticleSummaryExtractorConfig on ArticleSummaryItem (so that later on we know ArticleSummaryExtractor's name for setting series and default tag)

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/ArticleSummaryItem.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/ArticleSummaryItem.kt
@@ -1,12 +1,14 @@
 package net.dankito.newsreader.model
 
+import net.dankito.deepthought.model.ArticleSummaryExtractorConfig
 import net.dankito.newsreader.article.IArticleExtractor
 import java.util.*
 
 
 data class ArticleSummaryItem(val url : String, var title : String, val articleExtractorClass: Class<out IArticleExtractor>?,
                               var summary : String = "", var previewImageUrl : String? = null,
-                              var publishedDate: Date? = null, var updatedDate : Date? = null) {
+                              var publishedDate: Date? = null, var updatedDate : Date? = null,
+                              var articleSummaryExtractorConfig: ArticleSummaryExtractorConfig? = null) {
 
     private constructor() : this("", "", null) // for Jackson
 }
