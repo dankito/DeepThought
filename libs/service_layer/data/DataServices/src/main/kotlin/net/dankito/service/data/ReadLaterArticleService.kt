522863940fc374131967422cb54e7e58e40aba77
dankl <dankl@comfylight.com>
2017-07-12 15:30:14 +0200

    Not using separate Jackson instance anymore, using ISerializer again

--- libs/service_layer/data/DataServices/src/main/kotlin/net/dankito/service/data/ReadLaterArticleService.kt
+++ libs/service_layer/data/DataServices/src/main/kotlin/net/dankito/service/data/ReadLaterArticleService.kt
@@ -1,38 +1,20 @@
 package net.dankito.service.data
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.module.SimpleModule
 import net.dankito.deepthought.model.ReadLaterArticle
-import net.dankito.deepthought.model.Tag
 import net.dankito.deepthought.model.util.EntryExtractionResult
 import net.dankito.deepthought.service.data.DataManager
+import net.dankito.serializer.ISerializer
 import net.dankito.service.data.event.EntityChangedNotifier
-import net.dankito.service.data.serialization.PersistedTagDeserializer
-import net.dankito.service.data.serialization.PersistedTagSerializer
 
 
-class ReadLaterArticleService(dataManager: DataManager, entityChangedNotifier: EntityChangedNotifier, tagService: TagService)
+class ReadLaterArticleService(dataManager: DataManager, entityChangedNotifier: EntityChangedNotifier, private val serializer: ISerializer)
     : EntityServiceBase<ReadLaterArticle>(ReadLaterArticle::class.java, dataManager, entityChangedNotifier) {
-
-    private val objectMapper = ObjectMapper()
-
-    init {
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-
-        val module = SimpleModule()
-
-        module.addSerializer(Tag::class.java, PersistedTagSerializer())
-        module.addDeserializer(Tag::class.java, PersistedTagDeserializer(tagService))
-
-        objectMapper.registerModule(module)
-    }
 
 
     override fun onPrePersist(entity: ReadLaterArticle) {
         super.onPrePersist(entity)
 
-        entity.serializedEntryExtractionResult = objectMapper.writeValueAsString(entity.entryExtractionResult)
+        entity.serializedEntryExtractionResult = serializer.serializeObject(entity.entryExtractionResult)
     }
 
     override fun getAll(): List<ReadLaterArticle> {
@@ -54,7 +36,7 @@ class ReadLaterArticleService(dataManager: DataManager, entityChangedNotifier: E
 
     fun deserializeEntryExtractionResult(readLaterArticle: ReadLaterArticle) {
         if(readLaterArticle.entryExtractionResult.entry.content.isBlank()) { // entryExtractionResult not deserialized yet
-            readLaterArticle.entryExtractionResult = objectMapper.readValue(readLaterArticle.serializedEntryExtractionResult, EntryExtractionResult::class.java)
+            readLaterArticle.entryExtractionResult = serializer.deserializeObject(readLaterArticle.serializedEntryExtractionResult, EntryExtractionResult::class.java)
         }
     }
 
