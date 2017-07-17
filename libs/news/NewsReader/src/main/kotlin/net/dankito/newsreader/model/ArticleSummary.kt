5c0eb34f5304ab9fd97988d1addf60eee19701c4
dankl <dankl@comfylight.com>
2017-07-17 19:11:44 +0200

    Added indexOfAddedItems

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/ArticleSummary.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/model/ArticleSummary.kt
@@ -5,9 +5,16 @@ import kotlin.collections.ArrayList
 
 
 open class ArticleSummary(var articles: List<ArticleSummaryItem> = listOf(), var time : Date = Date(),
-                               var canLoadMoreItems: Boolean = false, var nextItemsUrl: String? = null) {
+                               var canLoadMoreItems: Boolean = false, var nextItemsUrl: String? = null, var indexOfAddedItems: Int = -1) {
 
     fun nextItemsLoaded(nextItemsSummary: ArticleSummary) {
+        if(nextItemsSummary.articles.isNotEmpty()) {
+            indexOfAddedItems = articles.size + 1
+        }
+        else {
+            indexOfAddedItems = -1
+        }
+
         val mergedArticles = ArrayList(articles)
         mergedArticles.addAll(nextItemsSummary.articles)
         articles = mergedArticles
