18d3af78965ba42226c580588962e180241318ac
dankl <dankl@comfylight.com>
2017-07-18 16:11:56 +0200

    Added nextActivitySetListeners

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/CurrentActivityTracker.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/CurrentActivityTracker.kt
@@ -1,10 +1,35 @@
 package net.dankito.deepthought.android.service
 
 import net.dankito.deepthought.android.activities.BaseActivity
+import java.util.concurrent.CopyOnWriteArrayList
 
 
 class CurrentActivityTracker {
 
     var currentActivity: BaseActivity? = null
+        set(value) {
+            field = value
+
+            if(value != null) {
+                callAndClearNextActivitySetListeners(value)
+            }
+        }
+
+
+    private val nextActivitySetListeners = CopyOnWriteArrayList<(BaseActivity) -> Unit>()
+
+    fun addNextActivitySetListener(listener: (BaseActivity) -> Unit) {
+        synchronized(nextActivitySetListeners) {
+            nextActivitySetListeners.add(listener)
+        }
+    }
+
+    private fun callAndClearNextActivitySetListeners(activity: BaseActivity) {
+        synchronized(nextActivitySetListeners) {
+            nextActivitySetListeners.forEach { it(activity) }
+
+            nextActivitySetListeners.clear()
+        }
+    }
 
 }
