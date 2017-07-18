bc0e3dd1cca041155755cad30bdfca9db9a3285a
dankl <dankl@comfylight.com>
2017-07-18 15:36:27 +0200

    Added EntryActivityParameters

---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/arguments/EntryActivityParameters.kt
@@ -0,0 +1,8 @@
+package net.dankito.deepthought.android.activities.arguments
+
+import net.dankito.deepthought.model.Entry
+import net.dankito.deepthought.model.ReadLaterArticle
+import net.dankito.deepthought.model.util.EntryExtractionResult
+
+
+data class EntryActivityParameters(val entry: Entry? = null, val readLaterArticle: ReadLaterArticle? = null, val entryExtractionResult: EntryExtractionResult? = null)
