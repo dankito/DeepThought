41406447c7010552902f07516df9e1d0a1c4063e
dankl <dankl@comfylight.com>
2017-07-18 14:56:38 +0200

    Added ActivityParameterHolder

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/BaseActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/BaseActivity.kt
@@ -1,6 +1,9 @@
 package net.dankito.deepthought.android.activities
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import net.dankito.deepthought.android.service.ActivityParameterHolder
 import net.dankito.deepthought.android.service.CurrentActivityTracker
 import javax.inject.Inject
 
@@ -10,6 +13,15 @@ open class BaseActivity : AppCompatActivity() {
     @Inject
     protected lateinit var currentActivityTracker: CurrentActivityTracker
 
+    @Inject
+    protected lateinit var parameterHolder: ActivityParameterHolder
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        parameterHolder.clearActivityResults() // a new Activity is started -> clear all results of previous activities
+    }
 
     override fun onStart() {
         super.onStart()
