b09392d51e9a24bffb30599770a10cc463e346a3
dankl <dankl@comfylight.com>
2017-07-24 13:42:41 +0200

    Added EditReferenceActivityParameters

---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/arguments/EditReferenceActivityParameters.kt
@@ -0,0 +1,6 @@
+package net.dankito.deepthought.android.activities.arguments
+
+import net.dankito.deepthought.model.Reference
+
+
+data class EditReferenceActivityParameters(val reference: Reference?)
