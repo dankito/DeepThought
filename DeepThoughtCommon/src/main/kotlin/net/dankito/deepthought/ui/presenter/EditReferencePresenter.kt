109d8f0783bf5584eb1c8d14917f594dc7c562ef
dankl <dankl@comfylight.com>
2017-07-24 17:37:10 +0200

    Added convertPublishingDateToText()

--- DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/EditReferencePresenter.kt
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/EditReferencePresenter.kt
@@ -2,9 +2,20 @@ package net.dankito.deepthought.ui.presenter
 
 import net.dankito.deepthought.model.Reference
 import net.dankito.deepthought.ui.presenter.util.ReferencePersister
+import java.text.DateFormat
+import java.util.*
 
 
 class EditReferencePresenter(private val referencePersister: ReferencePersister) {
+
+
+    fun convertPublishingDateToText(publishingDate: Date): String {
+        return getLongDateFormat().format(publishingDate)
+    }
+
+    private fun getLongDateFormat(): DateFormat {
+        return DateFormat.getDateInstance(DateFormat.LONG)
+    }
 
 
     fun saveReferenceAsync(reference: Reference, callback: (Boolean) -> Unit) {
