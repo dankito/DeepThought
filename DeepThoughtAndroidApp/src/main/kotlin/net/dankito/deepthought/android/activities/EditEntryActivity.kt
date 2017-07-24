9e8580e0e75ff2273ba9694f9f6968301b747c6e
dankl <dankl@comfylight.com>
2017-07-24 18:11:15 +0200

    Fixed bug that updateCanEntryBeSavedOnUIThread() hasn't been called when reference has been saved

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/EditEntryActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/EditEntryActivity.kt
@@ -166,6 +166,7 @@ class EditEntryActivity : BaseActivity() {
 
         entryExtractionResult?.let { it.reference = reference }
 
+        updateCanEntryBeSavedOnUIThread(true)
         setReferencePreviewOnUIThread()
     }
 
