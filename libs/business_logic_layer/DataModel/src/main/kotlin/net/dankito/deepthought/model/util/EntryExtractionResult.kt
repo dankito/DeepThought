9c926245f7a5568876f8a3f48a9c5f13e33a4c4a
dankl <dankl@comfylight.com>
2017-07-24 16:11:21 +0200

    Made reference settable

--- libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/util/EntryExtractionResult.kt
+++ libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/util/EntryExtractionResult.kt
@@ -5,7 +5,7 @@ import net.dankito.deepthought.model.Reference
 import net.dankito.deepthought.model.Tag
 
 
-data class EntryExtractionResult(val entry: Entry, val reference: Reference? = null, val tags: MutableList<Tag> = ArrayList()) {
+data class EntryExtractionResult(val entry: Entry, var reference: Reference? = null, val tags: MutableList<Tag> = ArrayList()) {
 
 
     private constructor() : this(Entry("")) // for Jackson
