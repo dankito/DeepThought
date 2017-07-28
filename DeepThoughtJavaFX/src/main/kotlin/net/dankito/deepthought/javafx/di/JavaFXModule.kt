6bd81ee20e08a86e3f5d5925efa3322d8f7ded9b
dankl <dankl@comfylight.com>
2017-07-28 19:52:58 +0200

    Added JavaFXNetworkConnectivityManager (TODO: implement)

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/di/JavaFXModule.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/di/JavaFXModule.kt
@@ -10,11 +10,14 @@ import net.dankito.deepthought.javafx.dialogs.mainwindow.MainWindowController
 import net.dankito.deepthought.javafx.routing.JavaFXRouter
 import net.dankito.deepthought.javafx.service.JavaFXClipboardService
 import net.dankito.deepthought.javafx.service.communication.JavaFXDeviceRegistrationHandler
+import net.dankito.deepthought.javafx.service.network.JavaFXNetworkConnectivityManager
 import net.dankito.deepthought.service.data.DataManager
 import net.dankito.deepthought.ui.IRouter
 import net.dankito.service.search.ISearchEngine
 import net.dankito.service.synchronization.initialsync.InitialSyncManager
 import net.dankito.utils.localization.Localization
+import net.dankito.utils.services.network.INetworkConnectivityManager
+import net.dankito.utils.services.network.NetworkHelper
 import net.dankito.utils.ui.IClipboardService
 import net.dankito.utils.ui.IDialogService
 import javax.inject.Singleton
@@ -60,6 +63,12 @@ class JavaFXModule(private val mainWindowController: MainWindowController) {
     @Singleton
     fun provideDeviceRegistrationHandler(dataManager: DataManager, initialSyncManager: InitialSyncManager, dialogService: IDialogService, localization: Localization) : IDeviceRegistrationHandler {
         return JavaFXDeviceRegistrationHandler(dataManager, initialSyncManager, dialogService, localization)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkConnectivityManager(networkHelper: NetworkHelper) : INetworkConnectivityManager {
+        return JavaFXNetworkConnectivityManager(networkHelper)
     }
 
 }
---
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/network/JavaFXNetworkConnectivityManager.kt
@@ -0,0 +1,11 @@
+package net.dankito.deepthought.javafx.service.network
+
+import net.dankito.utils.services.network.NetworkConnectivityManagerBase
+import net.dankito.utils.services.network.NetworkHelper
+
+
+class JavaFXNetworkConnectivityManager(networkHelper: NetworkHelper) : NetworkConnectivityManagerBase(networkHelper) {
+
+    // TODO: implement
+
+}
