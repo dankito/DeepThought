b1859b4ab3add41ecaf125e407577424a029b18a
dankl <dankl@comfylight.com>
2017-07-29 18:02:12 +0200

    Logging activity lifecycle

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/BaseActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import net.dankito.deepthought.android.service.ActivityParameterHolder
 import net.dankito.deepthought.android.service.CurrentActivityTracker
+import org.slf4j.LoggerFactory
 import javax.inject.Inject
 
 
@@ -11,6 +12,8 @@ open class BaseActivity : AppCompatActivity() {
 
     companion object {
         val ParametersId = "BASE_ACTIVITY_PARAMETERS_ID"
+
+        private val log = LoggerFactory.getLogger(BaseActivity::class.java)
     }
 
 
@@ -22,6 +25,7 @@ open class BaseActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        log.info("Creating Activity $javaClass")
         super.onCreate(savedInstanceState)
 
         parameterHolder.clearActivityResults() // a new Activity is started -> clear all results of previous activities
@@ -31,12 +35,21 @@ open class BaseActivity : AppCompatActivity() {
         super.onStart()
 
         currentActivityTracker.currentActivity = this
+
+        log.info("Started Activity $javaClass")
     }
 
     override fun onResume() {
         super.onResume()
 
         currentActivityTracker.currentActivity = this
+
+        log.info("Resumed Activity $javaClass")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log.info("Paused Activity $javaClass")
     }
 
     override fun onStop() {
@@ -45,6 +58,7 @@ open class BaseActivity : AppCompatActivity() {
         }
 
         super.onStop()
+        log.info("Stopped Activity $javaClass")
     }
 
     override fun onDestroy() {
@@ -53,6 +67,7 @@ open class BaseActivity : AppCompatActivity() {
         }
 
         super.onDestroy()
+        log.info("Destroyed Activity $javaClass")
     }
 
 
