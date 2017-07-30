bf515d96d464e7c922c0e69d8308c744e7ce120b
dankl <dankl@comfylight.com>
2017-07-30 12:39:23 +0200

    Added parameterless constructor for Jackson

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Version.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Version.kt
@@ -2,4 +2,6 @@ package net.dankito.utils.version
 
 
 class Version(val major: Int, val minor: Int, val patch: Int = 0, val build: String? = null) {
+
+    private constructor() : this(0, 0, 0) // for Jackson
 }
