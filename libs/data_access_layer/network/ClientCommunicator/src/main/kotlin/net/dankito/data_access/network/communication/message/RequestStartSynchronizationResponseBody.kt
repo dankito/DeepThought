c243788f7b921473200760b8b9f4be62e3b478c9
dankl <dankl@comfylight.com>
2017-07-30 12:47:18 +0200

    Added deviceInfo for when app versions are incompatible

--- libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/RequestStartSynchronizationResponseBody.kt
+++ libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/RequestStartSynchronizationResponseBody.kt
@@ -1,7 +1,7 @@
 package net.dankito.data_access.network.communication.message
 
 
-data class RequestStartSynchronizationResponseBody(val result: RequestStartSynchronizationResult, val synchronizationPort: Int = 0) {
+data class RequestStartSynchronizationResponseBody(val result: RequestStartSynchronizationResult, val synchronizationPort: Int = 0, val deviceInfo: DeviceInfo? = null) {
 
     private constructor() : this(RequestStartSynchronizationResult.DENIED)  // for Jackson
 
