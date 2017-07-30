aa4173ad60f63351736ec1a6ed5e8c4a5ac25324
dankl <dankl@comfylight.com>
2017-07-30 12:41:09 +0200

    Added appVersion and dataModelVersion to DeviceInfo

--- libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/DeviceInfo.kt
+++ libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/DeviceInfo.kt
@@ -1,18 +1,20 @@
 package net.dankito.data_access.network.communication.message
 
 import net.dankito.deepthought.model.Device
+import net.dankito.utils.version.Version
+import net.dankito.utils.version.Versions
 
 
-data class DeviceInfo(val id: String, val uniqueDeviceId: String) {
+data class DeviceInfo(val id: String, val uniqueDeviceId: String, val appVersion: Version, val dataModelVersion: Int) {
 
     companion object {
         fun fromDevice(device: Device): DeviceInfo {
-            return DeviceInfo(device.id ?: "", device.uniqueDeviceId)
+            return DeviceInfo(device.id ?: "", device.uniqueDeviceId, Versions.AppVersion, Versions.DataModelVersion)
         }
     }
 
 
-    private constructor() : this("", "") // for Jackson
+    private constructor() : this("", "", Version(0, 0), 0) // for Jackson
 
 
 }
