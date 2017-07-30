1a74af00678ade447f9f2c1226e91bdbdf7bae12
dankl <dankl@comfylight.com>
2017-07-30 12:01:54 +0200

    Added Versions

---
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Versions.kt
@@ -0,0 +1,16 @@
+package net.dankito.utils.version
+
+
+class Versions {
+
+    companion object {
+        val AppVersion = Version(0, 2)
+
+        const val DataModelVersion = 1
+
+        const val DataIndexVersion = 1
+
+        const val HtmlEditorVersion = 1
+    }
+
+}
