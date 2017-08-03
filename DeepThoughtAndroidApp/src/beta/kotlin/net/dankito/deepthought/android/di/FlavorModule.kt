fc76c1fd68788d95a3ce2780d0803404cb8a2e25
dankl <dankl@comfylight.com>
2017-08-03 14:52:09 +0200

    Removed Fabric

--- DeepThoughtAndroidApp/src/beta/kotlin/net/dankito/deepthought/android/di/FlavorModule.kt
+++ DeepThoughtAndroidApp/src/beta/kotlin/net/dankito/deepthought/android/di/FlavorModule.kt
@@ -2,7 +2,6 @@ package net.dankito.deepthought.android.di
 
 import dagger.Module
 import dagger.Provides
-import net.dankito.deepthought.android.reporting.FabricCrashReporter
 import net.dankito.deepthought.android.reporting.TestFairyCrashReporter
 import net.dankito.deepthought.android.service.reporting.ICrashReporter
 import net.dankito.newsreader.summary.IImplementedArticleSummaryExtractorsManager
