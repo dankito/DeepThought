a6c77f731c3b431d92d2e985a178f6537c0982d9
dankl <dankl@comfylight.com>
2017-08-15 16:01:40 +0200

    Added a context menu to edit, share url of and delete Reference

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/ReferencesListView.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/ReferencesListView.kt
@@ -1,6 +1,11 @@
 package net.dankito.deepthought.android.fragments
 
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.fragment_tab_references.view.*
 import net.dankito.deepthought.android.R
 import net.dankito.deepthought.android.adapter.ReferencesAdapter
 import net.dankito.deepthought.android.di.AppComponent
@@ -57,6 +62,50 @@ class ReferencesListView: MainActivityTabFragment(R.layout.fragment_tab_referenc
             presenter.showEntriesForReference(reference)
         }
     }
+
+
+    override fun setupUI(rootView: View?) {
+        super.setupUI(rootView)
+
+        rootView?.let {
+            registerForContextMenu(rootView.lstReferences)
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        activity?.menuInflater?.inflate(R.menu.list_item_reference_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        (item.menuInfo as? AdapterView.AdapterContextMenuInfo)?.position?.let { position ->
+            if(position > adapter.count) {
+                return super.onContextItemSelected(item)
+            }
+
+            val selectedReference = adapter.getItem(position)
+
+            when(item.itemId) {
+                R.id.mnEditReference -> {
+                    presenter.editReference(selectedReference)
+                    return true
+                }
+                R.id.mnShareReferenceUrl -> {
+                    presenter.copyReferenceUrlToClipboard(selectedReference)
+                    return true
+                }
+                R.id.mnDeleteReference -> {
+                    presenter.deleteReference(selectedReference)
+                    return true
+                }
+                else -> return super.onContextItemSelected(item)
+            }
+        }
+
+        return super.onContextItemSelected(item)
+    }
+
 
     override fun getQueryHint() = activity.getString(R.string.search_hint_references)
 
