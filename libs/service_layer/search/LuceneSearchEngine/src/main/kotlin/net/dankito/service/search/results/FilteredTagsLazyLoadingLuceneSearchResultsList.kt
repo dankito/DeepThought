ea6c0c579f50384250eacf54ae6dff1a14adac14
dankl <dankl@comfylight.com>
2017-08-15 13:30:38 +0200

    Made LazyLoadingLuceneSearchResultsList lazy loading its entityIds (loading them upfront would take way too long for big result lists, e.g. a large entry collection)

--- libs/service_layer/search/LuceneSearchEngine/src/main/kotlin/net/dankito/service/search/results/FilteredTagsLazyLoadingLuceneSearchResultsList.kt
+++ libs/service_layer/search/LuceneSearchEngine/src/main/kotlin/net/dankito/service/search/results/FilteredTagsLazyLoadingLuceneSearchResultsList.kt
@@ -3,27 +3,51 @@ package net.dankito.service.search.results
 import net.dankito.data_access.database.IEntityManager
 import net.dankito.deepthought.model.Entry
 import net.dankito.service.search.FieldName
+import org.apache.lucene.document.Document
 import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.ScoreDoc
+import org.slf4j.LoggerFactory
 
 
 class FilteredTagsLazyLoadingLuceneSearchResultsList(entityManager: IEntityManager, searcher: IndexSearcher, hits: Array<ScoreDoc>)
     : LazyLoadingLuceneSearchResultsList<Entry>(entityManager, searcher, Entry::class.java, FieldName.EntryId, hits) {
 
-    lateinit var tagIdsOnResultEntries: HashSet<String> // really bad code design as retrieveEntityIds() gets called in init {}, so tagIdsOnResultEntries is not initialized at
-    // the point of time when retrieveEntityIds() / getEntityIdFromHit() gets called
-
-
-    override fun retrieveEntityIds(hits: Array<ScoreDoc>): MutableCollection<String> {
-        tagIdsOnResultEntries = HashSet() // has to be initialized here as when hits is an empty array, getEntityIdFromHit() never gets called -> tagIdsOnResultEntries wouldn't get initialized
-
-        return super.retrieveEntityIds(hits)
+    companion object {
+        private val log = LoggerFactory.getLogger(FilteredTagsLazyLoadingLuceneSearchResultsList::class.java)
     }
 
-    override fun getEntityIdFromHit(hits: Array<ScoreDoc>, index: Int): String {
-        val hitDoc = searcher.doc(hits[index].doc)
 
-        val entityId = hitDoc.getField(idFieldName).stringValue()
+    var tagIdsOnResultEntries = HashSet<String>()
+
+
+    init {
+        try {
+            this.entityIds = retrieveEntityIds(hits)
+        } catch (ex: Exception) {
+            log.error("Could not retrieve results from hits", ex)
+        }
+
+    }
+
+    private fun retrieveEntityIds(hits: Array<ScoreDoc>): MutableCollection<String> {
+        val ids = LinkedHashSet<String>()
+
+        try {
+            for(index in hits.indices) {
+                val entityId = getEntityIdForIndex(index)
+
+                ids.add(entityId)
+            }
+        } catch (ex: Exception) {
+            log.error("Could not get all Entity IDs from Lucene Search Result", ex)
+        }
+
+        return ids
+    }
+
+
+    override fun getEntityIdFromHit(hitDoc: Document): String {
+        val entityId = super.getEntityIdFromHit(hitDoc)
 
         hitDoc.getFields(FieldName.EntryTagsIds).forEach { tagIdsOnResultEntries.add(it.stringValue()) }
 
--- libs/service_layer/search/LuceneSearchEngine/src/main/kotlin/net/dankito/service/search/results/LazyLoadingLuceneSearchResultsList.kt
+++ libs/service_layer/search/LuceneSearchEngine/src/main/kotlin/net/dankito/service/search/results/LazyLoadingLuceneSearchResultsList.kt
@@ -3,51 +3,26 @@ package net.dankito.service.search.results
 import net.dankito.data_access.database.IEntityManager
 import net.dankito.deepthought.model.BaseEntity
 import net.dankito.service.search.util.LazyLoadingList
+import org.apache.lucene.document.Document
 import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.ScoreDoc
-import org.slf4j.LoggerFactory
-import java.util.*
 
 
 open class LazyLoadingLuceneSearchResultsList<T : BaseEntity>(entityManager: IEntityManager, protected var searcher: IndexSearcher, resultType: Class<T>,
-                            protected var idFieldName: String, hits: Array<ScoreDoc>)
+                            protected var idFieldName: String, private val hits: Array<ScoreDoc>)
     : LazyLoadingList<T>(entityManager, resultType) {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(LazyLoadingLuceneSearchResultsList::class.java)
-    }
 
+    override val size: Int
+        get() = hits.size
 
-    init {
-        try {
-            this.entityIds = retrieveEntityIds(hits)
-        } catch (ex: Exception) {
-            log.error("Could not retrieve results from hits", ex)
-        }
-
-    }
-
-    protected open fun retrieveEntityIds(hits: Array<ScoreDoc>): MutableCollection<String> {
-        val ids = LinkedHashSet<String>()
-
-        try {
-            for(index in hits.indices) {
-                val entityId = getEntityIdFromHit(hits, index)
-
-                ids.add(entityId)
-            }
-        } catch (ex: Exception) {
-            log.error("Could not get all Entity IDs from Lucene Search Result", ex)
-        }
-
-        return ids
-    }
-
-    protected open fun getEntityIdFromHit(hits: Array<ScoreDoc>, index: Int): String {
+    override fun getEntityIdForIndex(index: Int): String {
         val hitDoc = searcher.doc(hits[index].doc)
-        val entityId = hitDoc.getField(idFieldName).stringValue()
+        return getEntityIdFromHit(hitDoc)
+    }
 
-        return entityId
+    protected open fun getEntityIdFromHit(hitDoc: Document): String {
+        return hitDoc.getField(idFieldName).stringValue()
     }
 
 }
