e786241377e2f26375e011698a6545cc8784d3b4
dankl <dankl@comfylight.com>
2017-08-17 16:38:21 +0200

    Added test for a real long multi page article

--- libs/news/NewsReader/src/test/kotlin/net/dankito/newsreader/article/TelepolisArticleExtractorTest.kt
+++ libs/news/NewsReader/src/test/kotlin/net/dankito/newsreader/article/TelepolisArticleExtractorTest.kt
@@ -23,7 +23,15 @@ class TelepolisArticleExtractorTest : ArticleExtractorTestBase() {
         getAndTestArticle("https://www.heise.de/tp/features/G20-Die-Eskalation-begann-mit-der-Ernennung-von-Dudde-zum-Polizeichef-3804738.html",
                 "G20: Die Eskalation begann mit der Ernennung von Dudde zum Polizeichef",
                 "Das Komitee für Grundrechte und Demokratie im Nachtrag zu den Auseinandersetzungen in Hamburg",
-                "https://www.heise.de/tp/imgs/89/2/2/6/0/6/1/3/Hamburgg20Polizei-f57f6ef5b9a089df.jpeg", 11900)
+                "https://www.heise.de/tp/imgs/89/2/2/6/0/6/1/3/Hamburgg20Polizei-f57f6ef5b9a089df.jpeg", 11700)
+    }
+
+    @Test
+    fun extractVeryLargeMultiPageArticle() {
+        getAndTestArticle("https://www.heise.de/tp/features/Googeln-Sie-mal-von-vorgestern-Mr-Damore-3804769.html",
+                "Googeln Sie mal \"von vorgestern\", Mr. Damore",
+                "Das im viel diskutierten \"Google Manifest\" formulierte Frauenbild scheint rückständig - ist jedoch aktueller denn je. Zeit für eine Debatte über einen neuen Typus \"sozial verträglicher Mann\"",
+                "https://www.heise.de/tp/imgs/89/2/2/6/0/6/4/0/_Johann_Wilhelm_von_der_Pfalz_und_Anna_Maria_Luisa_de__Medici__1708_-cc6af64ad3f9af0e.jpeg", 44600)
     }
 
 }
