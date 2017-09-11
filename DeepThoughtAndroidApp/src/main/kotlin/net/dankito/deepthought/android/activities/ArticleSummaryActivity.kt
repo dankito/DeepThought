8c37975e62d84a4734a94294f3b46c5b31e3d48e
dankl <dankl@comfylight.com>
2017-09-11 15:44:52 +0200

    Enabled fullscreen mode again now that scrolling in fullscreen works better

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/ArticleSummaryActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/ArticleSummaryActivity.kt
@@ -143,8 +143,8 @@ class ArticleSummaryActivity : BaseActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         rcyArticleSummaryItems.addItemDecoration(HorizontalDividerItemDecoration(this))
-//        rcyArticleSummaryItems.enterFullscreenModeListener = { recyclerViewEnteredFullscreenMode() }
-//        rcyArticleSummaryItems.leaveFullscreenModeListener = { recyclerViewLeftFullscreenMode() }
+        rcyArticleSummaryItems.enterFullscreenModeListener = { recyclerViewEnteredFullscreenMode() }
+        rcyArticleSummaryItems.leaveFullscreenModeListener = { recyclerViewLeftFullscreenMode() }
 
         rcyArticleSummaryItems.adapter = adapter
         adapter.itemClickListener = { item -> articleClicked(item) }
