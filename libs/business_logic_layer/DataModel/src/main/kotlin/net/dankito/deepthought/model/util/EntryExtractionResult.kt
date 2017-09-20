515d093edeed3c33a441a574ee0496637717f2f0
dankl <dankl@comfylight.com>
2017-09-20 15:12:45 +0200

    Adding now series not to reference anymore for EntryExtractionResult as this would add an unsaved reference to a persisted series (forgot in previous commit)

--- libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/util/EntryExtractionResult.kt
+++ libs/business_logic_layer/DataModel/src/main/kotlin/net/dankito/deepthought/model/util/EntryExtractionResult.kt
@@ -2,10 +2,11 @@ package net.dankito.deepthought.model.util
 
 import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Reference
+import net.dankito.deepthought.model.Series
 import net.dankito.deepthought.model.Tag
 
 
-data class EntryExtractionResult(var entry: Entry, var reference: Reference? = null, var tags: MutableList<Tag> = ArrayList(),
+data class EntryExtractionResult(var entry: Entry, var reference: Reference? = null, var series: Series? = null, var tags: MutableList<Tag> = ArrayList(),
                                  var couldExtractContent: Boolean = false,
                                  var webSiteHtml: String? = null,
                                  var error: Exception? = null) {
