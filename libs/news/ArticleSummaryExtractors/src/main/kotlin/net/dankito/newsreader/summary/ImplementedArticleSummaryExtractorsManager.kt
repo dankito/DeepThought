72f5b54dda0cb8e941393498627f423371ca9493
dankl <dankl@comfylight.com>
2017-09-29 10:50:30 +0200

    Added TelepolisArticleSummaryExtractor

--- libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/ImplementedArticleSummaryExtractorsManager.kt
+++ libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/ImplementedArticleSummaryExtractorsManager.kt
@@ -12,6 +12,7 @@ class ImplementedArticleSummaryExtractorsManager(val webClient: IWebClient) : II
         extractors.put(SueddeutscheMagazinArticleSummaryExtractor::class.java, SueddeutscheMagazinArticleSummaryExtractor(webClient))
         extractors.put(HeiseNewsArticleSummaryExtractor::class.java, HeiseNewsArticleSummaryExtractor(webClient))
         extractors.put(HeiseDeveloperArticleSummaryExtractor::class.java, HeiseDeveloperArticleSummaryExtractor(webClient))
+        extractors.put(TelepolisArticleSummaryExtractor::class.java, TelepolisArticleSummaryExtractor(webClient))
         extractors.put(NetzPolitikOrgArticleSummaryExtractor::class.java, NetzPolitikOrgArticleSummaryExtractor(webClient))
         extractors.put(DerFreitagArticleSummaryExtractor::class.java, DerFreitagArticleSummaryExtractor(webClient))
         extractors.put(GuardianArticleSummaryExtractor::class.java, GuardianArticleSummaryExtractor(webClient))
