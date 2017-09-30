7152017b14c04ffc4502d288854ba35619475d43
dankl <dankl@comfylight.com>
2017-09-30 20:39:19 +0200

    Removed unused ReferenceService

--- DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/util/SeriesPersister.kt
+++ DeepThoughtCommon/src/main/kotlin/net/dankito/deepthought/ui/presenter/util/SeriesPersister.kt
@@ -2,13 +2,12 @@ package net.dankito.deepthought.ui.presenter.util
 
 import net.dankito.deepthought.di.CommonComponent
 import net.dankito.deepthought.model.Series
-import net.dankito.service.data.ReferenceService
 import net.dankito.service.data.SeriesService
 import net.dankito.utils.IThreadPool
 import javax.inject.Inject
 
 
-class SeriesPersister(private val seriesService: SeriesService, private val referenceService: ReferenceService) {
+class SeriesPersister(private val seriesService: SeriesService) {
 
     @Inject
     protected lateinit var threadPool: IThreadPool
