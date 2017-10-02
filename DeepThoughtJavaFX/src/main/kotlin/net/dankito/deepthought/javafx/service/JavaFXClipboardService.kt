c28d9ba06d1e67c579166c27a8f8a71232a4ade0
dankl <dankl@comfylight.com>
2017-10-02 13:15:50 +0200

    Implemented copying url to clipboard

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/JavaFXClipboardService.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/JavaFXClipboardService.kt
@@ -1,14 +1,24 @@
 package net.dankito.deepthought.javafx.service
 
+import javafx.scene.input.Clipboard
+import javafx.scene.input.ClipboardContent
 import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Reference
 import net.dankito.deepthought.model.Series
 import net.dankito.utils.ui.IClipboardService
 
 
+
+
 class JavaFXClipboardService : IClipboardService {
 
     override fun copyUrlToClipboard(url: String) {
+        val content = ClipboardContent()
+
+        content.putString(url) // also copy URL as Plain Text
+        content.putUrl(url)
+
+        Clipboard.getSystemClipboard().setContent(content)
     }
 
     override fun copyEntryToClipboard(entry: Entry, reference: Reference?, series: Series?) {
