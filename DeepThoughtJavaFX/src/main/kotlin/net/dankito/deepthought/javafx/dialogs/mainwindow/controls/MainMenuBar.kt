e0cf3baefe139f0cba977fa1bbd32cca1719e719
dankl <dankl@comfylight.com>
2017-10-04 13:14:30 +0200

    Reduced indent

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/controls/MainMenuBar.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/controls/MainMenuBar.kt
@@ -33,64 +33,64 @@ class MainMenuBar : View() {
     }
 
     override val root = gridpane {
-                row {
-                    menubar {
-                        minHeight = 30.0
-                        maxHeight = 30.0
+        row {
+            menubar {
+                minHeight = 30.0
+                maxHeight = 30.0
 
-                        gridpaneColumnConstraints {
-                            hgrow = Priority.ALWAYS
-                            minWidth = 50.0
-                        }
-
-                        menu(messages["main.window.menu.file"]) {
-                            mnitmFileClipboard = menu(messages["main.window.menu.clipboard"])
-
-                            separator()
-
-                            item(messages["main.window.menu.quit"], KeyCombination.keyCombination("Ctrl+Q")) {
-                                action { primaryStage.close() }
-                            }
-                        }
-
-                        menu(messages["main.window.menu.view"]) {
-                            isVisible = false
-                            checkmenuitem(messages["main.window.menu.view.show.quick.edit.entry.pane"], KeyCombination.keyCombination("F4")) {
-                                selectedProperty().addListener(ChangeListener<Boolean> { observable, oldValue, newValue ->
-
-                                })
-                            }
-                        }
-
-                        menu(messages["main.window.menu.tools"]) {
-                            isVisible = false
-                            menu(messages["main.window.menu.tools.language"]) {
-                                checkmenuitem(messages["application.language.english"]) {
-                                    action { }
-                                }
-                                checkmenuitem(messages["application.language.german"]) {
-                                    action { }
-                                }
-                            }
-                        }
-
-                        menu(messages["main.window.menu.window"]) {
-                            isVisible = false
-                        }
-
-                        menu(messages["main.window.menu.help"]) {
-                            isVisible = false
-                            item(messages["main.window.menu.help.about"]) {
-                                action { }
-                            }
-                        }
-                    }
-
-                    add(ArticleExtractorsMenuButton())
+                gridpaneColumnConstraints {
+                    hgrow = Priority.ALWAYS
+                    minWidth = 50.0
                 }
 
-                clipboardWatcher = JavaFXClipboardWatcher(primaryStage) { clipboardContentChangedExternally(it) }
+                menu(messages["main.window.menu.file"]) {
+                    mnitmFileClipboard = menu(messages["main.window.menu.clipboard"])
+
+                    separator()
+
+                    item(messages["main.window.menu.quit"], KeyCombination.keyCombination("Ctrl+Q")) {
+                        action { primaryStage.close() }
+                    }
+                }
+
+                menu(messages["main.window.menu.view"]) {
+                    isVisible = false
+                    checkmenuitem(messages["main.window.menu.view.show.quick.edit.entry.pane"], KeyCombination.keyCombination("F4")) {
+                        selectedProperty().addListener(ChangeListener<Boolean> { observable, oldValue, newValue ->
+
+                        })
+                    }
+                }
+
+                menu(messages["main.window.menu.tools"]) {
+                    isVisible = false
+                    menu(messages["main.window.menu.tools.language"]) {
+                        checkmenuitem(messages["application.language.english"]) {
+                            action { }
+                        }
+                        checkmenuitem(messages["application.language.german"]) {
+                            action { }
+                        }
+                    }
+                }
+
+                menu(messages["main.window.menu.window"]) {
+                    isVisible = false
+                }
+
+                menu(messages["main.window.menu.help"]) {
+                    isVisible = false
+                    item(messages["main.window.menu.help.about"]) {
+                        action { }
+                    }
+                }
             }
+
+            add(ArticleExtractorsMenuButton())
+        }
+
+        clipboardWatcher = JavaFXClipboardWatcher(primaryStage) { clipboardContentChangedExternally(it) }
+    }
 
 
     private fun clipboardContentChangedExternally(clipboardContent: JavaFXClipboardContent) {
