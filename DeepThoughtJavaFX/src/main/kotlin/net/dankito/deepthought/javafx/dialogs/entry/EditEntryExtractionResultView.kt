b9502c9eec470ea9a7893376c9edaa713a3bf92d
dankl <dankl@comfylight.com>
2017-10-04 11:24:20 +0200

    Implemented showing url if content couldn't get extracted and when after html of url has been retrieved content can get extracted showing content then

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/entry/EditEntryExtractionResultView.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/entry/EditEntryExtractionResultView.kt
@@ -1,15 +1,41 @@
 package net.dankito.deepthought.javafx.dialogs.entry
 
+import net.dankito.deepthought.javafx.di.AppComponent
 import net.dankito.deepthought.model.util.EntryExtractionResult
+import net.dankito.deepthought.news.article.ArticleExtractorManager
+import javax.inject.Inject
 
 
 class EditEntryExtractionResultView : EditEntryViewBase() {
+
+    @Inject
+    protected lateinit var articleExtractorManager: ArticleExtractorManager
+
 
     val extractionResult: EntryExtractionResult by param()
 
 
     init {
-        showData(extractionResult.entry, extractionResult.tags, extractionResult.reference, extractionResult.series)
+        AppComponent.component.inject(this)
+
+        showExtractionResult()
+    }
+
+
+    private fun showExtractionResult() {
+        val content = if (extractionResult.couldExtractContent) extractionResult.entry.content else ""
+        showData(extractionResult.entry, extractionResult.tags, extractionResult.reference, extractionResult.series, content)
+    }
+
+
+    override fun urlLoaded(url: String, html: String) {
+        super.urlLoaded(url, html)
+
+        articleExtractorManager.extractArticleAndAddDefaultData(extractionResult, html, url)
+
+        if(extractionResult.couldExtractContent) { // now content could get extracted, therefore show it
+            showExtractionResult()
+        }
     }
 
 
