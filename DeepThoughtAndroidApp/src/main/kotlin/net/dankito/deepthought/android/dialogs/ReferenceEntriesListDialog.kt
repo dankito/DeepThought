df728b8995160a06b8925c2a824344e7f449f535
dankl <dankl@comfylight.com>
2017-10-05 21:49:13 +0200

    Fixed restoring ReferenceEntriesListDialog

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/dialogs/ReferenceEntriesListDialog.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/dialogs/ReferenceEntriesListDialog.kt
@@ -1,24 +1,38 @@
 package net.dankito.deepthought.android.dialogs
 
-import android.content.DialogInterface
+import android.os.Bundle
 import android.support.v4.app.FragmentManager
+import net.dankito.deepthought.android.di.AppComponent
 import net.dankito.deepthought.model.Entry
 import net.dankito.deepthought.model.Reference
+import net.dankito.service.data.ReferenceService
 import net.dankito.service.data.messages.ReferenceChanged
 import net.dankito.service.search.specific.EntriesSearch
 import net.engio.mbassy.listener.Handler
+import javax.inject.Inject
 
 
 class ReferenceEntriesListDialog: EntriesListDialogBase() {
 
     companion object {
         val TAG: String = javaClass.name
+
+        private const val REFERENCE_ID_EXTRA_NAME = "REFERENCE_ID"
     }
+
+
+    @Inject
+    protected lateinit var referenceService: ReferenceService
 
 
     private lateinit var reference: Reference
 
     private val eventBusListener = EventBusListener()
+
+
+    init {
+        AppComponent.component.inject(this)
+    }
 
 
     override fun getDialogTag() = TAG
@@ -27,15 +41,38 @@ class ReferenceEntriesListDialog: EntriesListDialogBase() {
     fun showDialog(fragmentManager: FragmentManager, reference: Reference) {
         this.reference = reference
 
-        eventBus.register(eventBusListener)
-
         showDialog(fragmentManager)
     }
 
-    override fun onDismiss(dialog: DialogInterface?) {
+    override fun onResume() {
+        super.onResume()
+
+        eventBus.register(eventBusListener)
+    }
+
+    override fun onPause() {
         eventBus.unregister(eventBusListener)
 
-        super.onDismiss(dialog)
+        super.onPause()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.let {
+            outState.putString(REFERENCE_ID_EXTRA_NAME, reference.id)
+        }
+    }
+
+    override fun restoreState(savedInstanceState: Bundle) {
+        super.restoreState(savedInstanceState)
+
+        savedInstanceState.getString(REFERENCE_ID_EXTRA_NAME)?.let { referenceId ->
+            referenceService.retrieve(referenceId)?.let {
+                this.reference = it
+            }
+        }
     }
 
 
