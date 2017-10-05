ea76bec059c00430cb7dd5afc2bf0fe798097331
dankl <dankl@comfylight.com>
2017-10-05 11:32:19 +0200

    Added TestUtil

---
+++ DeepThoughtAndroidApp/src/androidTest/java/net/dankito/deepthought/android/testutil/TestUtil.kt
@@ -0,0 +1,14 @@
+package net.dankito.deepthought.android.testutil
+
+
+class TestUtil {
+
+    companion object {
+
+        fun sleep(millis: Long) {
+            Thread.sleep(millis)
+        }
+
+    }
+
+}
