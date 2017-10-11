cbe1334d48af46626ae68515536b79a2c26be09f
dankl <dankl@comfylight.com>
2017-10-11 19:23:20 +0200

    Added export() to IDataExporter

--- libs/service_layer/utils/DeepThoughtBaseLib/src/main/kotlin/net/dankito/deepthought/service/import_export/IDataExporter.kt
+++ libs/service_layer/utils/DeepThoughtBaseLib/src/main/kotlin/net/dankito/deepthought/service/import_export/IDataExporter.kt
@@ -1,9 +1,15 @@
 package net.dankito.deepthought.service.import_export
 
+import net.dankito.deepthought.model.Entry
+import java.io.File
+
 
 interface IDataExporter {
 
     val name: String
         get
+
+
+    fun export(destinationFile: File, entries: Collection<Entry>)
 
 }
