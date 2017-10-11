25d4e81cfa694ea0963ec50fdd7e828082fa2815
dankl <dankl@comfylight.com>
2017-10-11 11:03:02 +0200

    Added checking <meta name="title"> for title

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaDataExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaDataExtractor.kt
@@ -35,6 +35,8 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
 
         document.head().select("meta[property=\"og:title\"]").first()?.attr("content")?.let { return it }
 
+        document.head().select("meta[name=\"title\"]").first()?.attr("content")?.let { return it }
+
         return document.title()
     }
 
