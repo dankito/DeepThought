7a0344f918ba2e79a4096f6ce6c963398ff60db9
dankl <dankl@comfylight.com>
2017-10-27 00:00:54 +0200

    Fixed checking for http://jetzt.de/ urls

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/SueddeutscheJetztArticleExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/SueddeutscheJetztArticleExtractor.kt
@@ -26,7 +26,8 @@ class SueddeutscheJetztArticleExtractor(webClient: IWebClient) : ArticleExtracto
 
     override fun canExtractEntryFromUrl(url: String): Boolean {
         return (url.toLowerCase().startsWith("http://www.jetzt.de/") && url.length > "http://www.jetzt.de/".length) ||
-                (url.toLowerCase().startsWith("http://jetzt.sueddeutsche.de/") && url.length > "http://jetzt.sueddeutsche.de/".length)
+                (url.toLowerCase().startsWith("http://jetzt.sueddeutsche.de/") && url.length > "http://jetzt.sueddeutsche.de/".length) ||
+                (url.toLowerCase().startsWith("http://jetzt.de/") && url.length > "http://jetzt.de/".length)
     }
 
     override fun parseHtmlToArticle(extractionResult: ItemExtractionResult, document: Document, url: String) {
