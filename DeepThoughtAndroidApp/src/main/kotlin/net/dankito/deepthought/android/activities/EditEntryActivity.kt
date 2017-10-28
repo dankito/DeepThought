60e3c307844c490298ccfcdcea428f652c4d36d1
dankl <dankl@comfylight.com>
2017-10-28 14:37:55 +0200

    Also removed checking if content has been edited

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/EditEntryActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/EditEntryActivity.kt
@@ -496,7 +496,7 @@ class EditEntryActivity : BaseActivity() {
         val localSettings = entryService.dataManager.localSettings
 
         if(localSettings.didShowSaveItemChangesHelp == false &&
-                (contentToEdit.isNullOrBlank() == false || tagsOnEntry.size > 0 || sourceToEdit != null)) { // only show context help when really a value is set (summary should be obvious)
+                (tagsOnEntry.size > 0 || sourceToEdit != null)) { // only show context help when really a value is set (summary should be obvious)
             contextHelpUtil.showContextHelp(lytContextHelpSave, R.string.context_help_save_item_changes)
 
             localSettings.didShowSaveItemChangesHelp = true
