b19558b8492a04d6cc0f2d4f251e6f531d737c1e
dankl <dankl@comfylight.com>
2017-10-29 16:33:44 +0100

    Re-added Fabric

---
+++ DeepThoughtAndroidApp/src/beta/kotlin/net/dankito/deepthought/android/reporting/FabricCrashReporter.kt
@@ -0,0 +1,28 @@
+package net.dankito.deepthought.android.reporting
+
+import android.content.Context
+import com.crashlytics.android.BuildConfig
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
+import net.dankito.deepthought.android.service.reporting.ICrashReporter
+import net.dankito.deepthought.service.data.DataManager
+
+
+class FabricCrashReporter : ICrashReporter {
+
+    override fun init(context: Context, dataManager: DataManager) {
+        // Set up Crashlytics, disabled for debug builds (see https://docs.fabric.io/android/crashlytics/build-tools.html)
+        val crashlyticsKit = Crashlytics.Builder()
+                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build()
+
+        Fabric.with(context, crashlyticsKit)
+
+        dataManager.addInitializationListener {
+            Crashlytics.setUserIdentifier(dataManager.localDevice.id)
+            Crashlytics.setUserName(dataManager.localUser.id)
+        }
+    }
+
+}
