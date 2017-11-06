6bf4d4e9390c9397ebf2e5c7e491ca7548a26647
dankl <dankl@comfylight.com>
2017-11-06 16:13:48 +0100

    Implemented extracting author and keywords Added additional elements for title, description, siteName, publishingDate and previewImageUrl

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaData.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaData.kt
@@ -1,8 +1,10 @@
 package net.dankito.newsreader.article
 
 import java.util.*
+import kotlin.collections.HashSet
 
 
 data class WebPageMetaData(var title: String? = null, var description: String? = null,
                            var siteName: String? = null, var previewImageUrl: String? = null,
+                           var author: String? = null, var keywords: MutableSet<String> = HashSet(),
                            var publishingDateString: String? = null, var publishingDate: Date? = null)
--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaDataExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/WebPageMetaDataExtractor.kt
@@ -4,6 +4,7 @@ import net.dankito.data_access.network.webclient.IWebClient
 import net.dankito.data_access.network.webclient.extractor.ExtractorBase
 import org.jsoup.nodes.Document
 import java.util.*
+import kotlin.collections.HashSet
 
 
 class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient) {
@@ -18,6 +19,10 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
         metaData.siteName = extractSiteName(document)
 
         metaData.previewImageUrl = extractPreviewImageUrl(document)
+
+        metaData.author = extractAuthor(document)
+
+        metaData.keywords = extractKeywords(document)
 
         metaData.publishingDateString = extractPublishingDateString(document)
         metaData.publishingDateString?.let {
@@ -39,6 +44,10 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
 
         document.head().select("meta[name=\"fulltitle\"]").first()?.attr("content")?.let { return it }
 
+        document.head().select("meta[itemprop=\"alternativeHeadline\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"hdl\"]").first()?.attr("content")?.let { return it }
+
         return document.title()
     }
 
@@ -48,6 +57,8 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
         document.head().select("meta[name=\"og:description\"]").first()?.attr("content")?.let { return it }
 
         document.head().select("meta[property=\"og:description\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"DC.description\"]").first()?.attr("content")?.let { return it }
 
         return null
     }
@@ -68,7 +79,59 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
             return it
         }
 
+        document.head().select("meta[name=\"apple-mobile-web-app-title\"]").first()?.attr("content")?.let { return it }
+
         return null
+    }
+
+    private fun extractAuthor(document: Document): String? {
+        // TODO: there can be multiple author elements
+        document.head().select("meta[name=\"author\"]").first()?.attr("content")?.let { return it }
+
+        // TODO: there can be multiple article:author elements
+        document.head().select("meta[property=\"article:author\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"publisher\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"twitter:app:name:googleplay\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"twitter:app:name:iphone\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"twitter:app:name:ipad\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[property=\"al:android:app_name\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[property=\"al:iphone:app_name\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[property=\"al:ipad:app_name\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[name=\"copyright\"]").first()?.attr("content")?.let { return it }
+
+        return null
+    }
+
+    private fun extractKeywords(document: Document): MutableSet<String> {
+        val keywords = HashSet<String>()
+
+        document.head().select("meta[name=\"keywords\"]").first()?.attr("content")?.let {
+            extractKeywords(it, keywords)
+        }
+
+        document.head().select("meta[name=\"news_keywords\"]").first()?.attr("content")?.let {
+            extractKeywords(it, keywords)
+        }
+
+        document.head().select("meta[property=\"article:tag\"]").first()?.attr("content")?.let {
+            extractKeywords(it, keywords)
+        }
+
+        return keywords
+    }
+
+    private fun extractKeywords(keywordsString: String, keywords: MutableSet<String>) {
+        keywordsString.split(",").map { it.trim() }.forEach {
+            keywords.add(it)
+        }
     }
 
     private fun extractPublishingDateString(document: Document): String? {
@@ -88,6 +151,8 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
 
         document.head().select("meta[property=\"article:published_time\"]").first()?.attr("content")?.let { return it }
 
+        document.head().select("meta[name=\"DC.date.issued\"]").first()?.attr("content")?.let { return it }
+
         return null
     }
 
@@ -105,6 +170,8 @@ class WebPageMetaDataExtractor(webClient: IWebClient) : ExtractorBase(webClient)
         document.head().select("meta[name=\"og:image\"]").first()?.attr("content")?.let { return it }
 
         document.head().select("meta[property=\"og:image\"]").first()?.attr("content")?.let { return it }
+
+        document.head().select("meta[property=\"twitter:image\"]").first()?.attr("content")?.let { return it }
 
         return null
     }
