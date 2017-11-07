f1d6e07bcbf5caf1999703168b6513544151a04e
dankl <dankl@comfylight.com>
2017-11-07 17:31:27 +0100

    Started ChefkochArticleExtractor

---
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/recipes/ChefkochArticleExtractor.kt
@@ -0,0 +1,57 @@
+package net.dankito.newsreader.article.recipes
+
+import net.dankito.data_access.network.webclient.IWebClient
+import net.dankito.deepthought.model.Item
+import net.dankito.deepthought.model.Source
+import net.dankito.deepthought.model.util.ItemExtractionResult
+import net.dankito.newsreader.article.ArticleExtractorBase
+import org.jsoup.nodes.Document
+
+
+class ChefkochArticleExtractor(webClient: IWebClient) : ArticleExtractorBase(webClient) {
+
+    override fun getName(): String? {
+        return "Chefkoch"
+    }
+
+    override fun canExtractEntryFromUrl(url: String): Boolean {
+        return url.contains("://www.chefkoch.de/rezepte/") && url.length > "http://www.chefkoch.de/rezepte/".length + 4
+    }
+
+
+    override fun parseHtmlToArticle(extractionResult: ItemExtractionResult, document: Document, url: String) {
+        document.body().select("div.content-wrapper.clearfix").first()?.let { contentWrapper ->
+            var title = ""
+            var summary = ""
+            var content = ""
+
+            contentWrapper.select("h1.page-title").first()?.let { pageTitle ->
+                title = pageTitle.text().trim()
+                content += pageTitle.outerHtml()
+            }
+
+            contentWrapper.select("div.summary").first()?.let { summaryElement ->
+                summary = summaryElement.text().trim()
+                content += summaryElement.outerHtml()
+            }
+
+            // TODO: extract images
+//            content += contentWrapper.select("#slideshow").first()?.outerHtml() ?: ""
+
+            contentWrapper.select("div.main-content").first()?.let { mainContent ->
+                mainContent.select("#text-ads-rezeptbild, .how2video-container, .mobile-only, .order-online, .recipe2shoppinglist," +
+                        "#text-ads-unter-zubereitung, #recipe-com-user-footer, .sfs-esi-positioning-container, #rezeptvideos").remove()
+
+                // TODO: may also get videos to work, remove #rezeptvideos then from above's content cleaning
+
+                // TODO: get ingredient form to work
+                mainContent.select("#incredientform").remove()
+
+                content += mainContent.outerHtml()
+            }
+
+            extractionResult.setExtractedContent(Item(content, summary), Source(title, url))
+        }
+    }
+
+}
---
+++ libs/news/NewsReader/src/test/kotlin/net/dankito/newsreader/article/recipes/ChefkochArticleExtractorTest.kt
@@ -0,0 +1,25 @@
+package net.dankito.newsreader.article.recipes
+
+import net.dankito.data_access.network.webclient.IWebClient
+import net.dankito.newsreader.article.ArticleExtractorTestBase
+import net.dankito.newsreader.article.IArticleExtractor
+import org.junit.Test
+
+class ChefkochArticleExtractorTest : ArticleExtractorTestBase() {
+
+    override fun createArticleExtractor(webClient: IWebClient): IArticleExtractor {
+        return ChefkochArticleExtractor(webClient)
+    }
+
+
+
+
+    @Test
+    fun importBlattspinatLasagneRecipe() {
+        getAndTestArticle("http://www.chefkoch.de/rezepte/2222841355733815/Spinat-Lasagne-mit-dreierlei-Sossen.html",
+                "Spinat-Lasagne mit dreierlei Soßen",
+                "simples, aber köstliches Lasagnerezept mit Blattspinat",
+                null, 8300, true)
+    }
+
+}
