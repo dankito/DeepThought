642cfe03bb18a62e245ac179cf4bb5c99557b548
dankl <dankl@comfylight.com>
2017-11-15 09:38:10 +0100

    Fixed parsing html

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/extensions/StringExtensions.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/extensions/StringExtensions.kt
@@ -5,7 +5,8 @@ import org.jsoup.Jsoup
 
 fun String?.getPlainTextForHtml(): String {
     if(this != null) {
-        return Jsoup.parseBodyFragment(this).text()
+        val textWithTags = Jsoup.parseBodyFragment(this).text()
+        return Jsoup.parse(textWithTags).text() // don't know why but html has to be parsed twice in this way to remove tags like <img>, see https://github.com/jhy/jsoup/issues/125
     }
 
     return ""
