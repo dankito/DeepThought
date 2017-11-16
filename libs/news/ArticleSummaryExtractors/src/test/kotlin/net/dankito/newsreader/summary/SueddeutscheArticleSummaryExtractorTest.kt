ef113f68d47425e0caac556ecdc6cfe99af0162a
dankl <dankl@comfylight.com>
2017-11-16 20:05:46 +0100

    Implemented loading more articles

--- libs/news/ArticleSummaryExtractors/src/test/kotlin/net/dankito/newsreader/summary/SueddeutscheArticleSummaryExtractorTest.kt
+++ libs/news/ArticleSummaryExtractors/src/test/kotlin/net/dankito/newsreader/summary/SueddeutscheArticleSummaryExtractorTest.kt
@@ -1,11 +1,56 @@
 package net.dankito.newsreader.summary
 
 import net.dankito.data_access.network.webclient.IWebClient
+import net.dankito.newsreader.model.ArticleSummary
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class SueddeutscheArticleSummaryExtractorTest : ArticleSummaryExtractorTestBase() {
 
     override fun createArticleSummaryExtractor(webClient: IWebClient): IArticleSummaryExtractor {
         return SueddeutscheArticleSummaryExtractor(webClient)
+    }
+
+
+    override fun testCanLoadMoreItems(summary: ArticleSummary) {
+        if(summary.articles.size < 100) { // loaded articles from home page
+            Assert.assertThat(summary.canLoadMoreItems, CoreMatchers.`is`(true))
+        }
+        else { // did load more items
+            Assert.assertThat(summary.canLoadMoreItems, CoreMatchers.`is`(false))
+        }
+
+        Assert.assertThat(summary.nextItemsUrl?.contains("://www.sueddeutsche.de"), CoreMatchers.`is`(true))
+    }
+
+
+    override fun urlHasHttpsPrefix(): Boolean {
+        return false
+    }
+
+
+
+    @Test
+    fun loadMoreItems() {
+        var summary: ArticleSummary? = null
+        val countDownLatch = CountDownLatch(1)
+
+        underTest.extractSummaryAsync {
+            it.result?.let { extractSummaryResult ->
+                underTest.loadMoreItemsAsync(extractSummaryResult) {
+                    summary = it.result
+
+                    countDownLatch.countDown()
+                }
+            }
+        }
+
+        countDownLatch.await(31, TimeUnit.SECONDS)
+
+        testSummary(summary)
     }
 
 }
