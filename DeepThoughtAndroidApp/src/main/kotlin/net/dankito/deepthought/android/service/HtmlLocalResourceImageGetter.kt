5d208ae05efa0dd801fa01bb78b90bca1432bb42
dankl <dankl@comfylight.com>
2017-11-25 13:57:01 +0100

    Fixed that setting color filter in HtmlLocalResourceImageGetter also affects all other instances of that Drawable

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/HtmlLocalResourceImageGetter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/HtmlLocalResourceImageGetter.kt
@@ -14,14 +14,17 @@ class HtmlLocalResourceImageGetter(private val context: Context, private val tin
             val resourceId = context.resources.getIdentifier(source , "drawable", context.packageName)
             if(resourceId > 0) { // 0 == resource not found
                 val drawable = ContextCompat.getDrawable(context, resourceId)
+                // drawables use the constantState as a cache -> if i apply a filter here then all instances, e.g. in SwipeButtons, of that drawable will be changed as well -> mutate. Thanks to https://stackoverflow.com/a/18542972 for this tip
+                val mutableDrawable = drawable.mutate()
+
                 val size = (16 * context.resources.displayMetrics.density).toInt() // TODO: this is bad applying a fixed size of 16
-                drawable.setBounds(0, 0, size, size) // thanks to Pratik Sharma for pointing that out to me, otherwise it won't get displayed (https://stackoverflow.com/a/14379821/8837882)
+                mutableDrawable.setBounds(0, 0, size, size) // thanks to Pratik Sharma for pointing that out to me, otherwise it won't get displayed (https://stackoverflow.com/a/14379821/8837882)
 
                 tintColor?.let {
-                    drawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
+                    mutableDrawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
                 }
 
-                return drawable
+                return mutableDrawable
             }
         }
 
