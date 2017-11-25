ec1c0de0d2c4458113d1f6c213475a1b620c5b86
dankl <dankl@comfylight.com>
2017-11-25 13:40:07 +0100

    Setting image's tint color to txtOnboardingText's text color

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/MainActivityTabFragment.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/fragments/MainActivityTabFragment.kt
@@ -314,7 +314,7 @@ abstract class MainActivityTabFragment<T : BaseEntity>(private val contextualAct
         txtOnboardingText?.let { txtOnboardingText ->
             lytOnboardingText?.visibility = View.VISIBLE
 
-            txtOnboardingText.text = contextHelpUtil.stringUtil.getSpannedFromHtmlWithImages(txtOnboardingText.context.getText(onboardingTextResourceId).toString(), context, android.R.color.black)
+            txtOnboardingText.text = contextHelpUtil.stringUtil.getSpannedFromHtmlWithImages(context.getText(onboardingTextResourceId).toString(), context, txtOnboardingText.currentTextColor)
         }
 
         searchMenuItem?.isVisible = false
--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/HtmlLocalResourceImageGetter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/HtmlLocalResourceImageGetter.kt
@@ -3,12 +3,11 @@ package net.dankito.deepthought.android.service
 import android.content.Context
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.text.Html
 
 
-class HtmlLocalResourceImageGetter(private val context: Context, private val tintColorResourceId: Int? = null) : Html.ImageGetter {
+class HtmlLocalResourceImageGetter(private val context: Context, private val tintColor: Int? = null) : Html.ImageGetter {
 
     override fun getDrawable(source: String?): Drawable? {
         source?.let {
@@ -18,24 +17,15 @@ class HtmlLocalResourceImageGetter(private val context: Context, private val tin
                 val size = (16 * context.resources.displayMetrics.density).toInt() // TODO: this is bad applying a fixed size of 16
                 drawable.setBounds(0, 0, size, size) // thanks to Pratik Sharma for pointing that out to me, otherwise it won't get displayed (https://stackoverflow.com/a/14379821/8837882)
 
-                maySetTintColor(drawable, tintColorResourceId)
+                tintColor?.let {
+                    drawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_ATOP)
+                }
 
                 return drawable
             }
         }
 
         return null
-    }
-
-    private fun maySetTintColor(drawable: Drawable, tintColorResourceId: Int?) {
-        tintColorResourceId?.let {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                drawable.setColorFilter(context.getColor(tintColorResourceId), PorterDuff.Mode.SRC_ATOP)
-            }
-            else {
-                drawable.setColorFilter(context.resources.getColor(tintColorResourceId), PorterDuff.Mode.SRC_ATOP)
-            }
-        }
     }
 
 }
