2aa3e71ff2c7458238e8e72692bff25e35d2f440
dankl <dankl@comfylight.com>
2017-11-26 14:22:12 +0100

    Added getCurrentActivity()

--- DeepThoughtAndroidApp/src/androidTest/java/net/dankito/deepthought/android/DeepThoughtActivityTestRule.kt
+++ DeepThoughtAndroidApp/src/androidTest/java/net/dankito/deepthought/android/DeepThoughtActivityTestRule.kt
@@ -1,10 +1,33 @@
 package net.dankito.deepthought.android
 
 import android.app.Activity
+import android.support.test.InstrumentationRegistry.getInstrumentation
+import android.support.test.espresso.core.deps.guava.collect.Iterables
 import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import android.support.test.runner.lifecycle.Stage
 
 
 class DeepThoughtActivityTestRule<T : Activity>(activityClass: Class<T>) : ActivityTestRule<T>(activityClass) {
+
+
+    /**
+     * Returns the currently displayed Activity.
+     * @return
+     */
+    fun getCurrentActivity(): Activity {
+        getInstrumentation().waitForIdleSync()
+        val activity = arrayOfNulls<Activity>(1)
+
+        getInstrumentation().runOnMainSync(Runnable {
+            val activities = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
+            if (activities.size > 0) {
+                activity[0] = Iterables.getOnlyElement(activities)
+            }
+        })
+
+        return activity[0]!!
+    }
 
 
 }
