2aa1362805b2b4f275f8d0704b869baffad151c0
dankl <dankl@comfylight.com>
2017-11-28 13:21:30 +0100

    Added NoOpSyncManager

---
+++ libs/service_layer/synchronization/SyncManager/src/main/kotlin/net/dankito/service/synchronization/NoOpSyncManager.kt
@@ -0,0 +1,31 @@
+package net.dankito.service.synchronization
+
+import net.dankito.deepthought.model.Device
+import net.dankito.deepthought.model.DiscoveredDevice
+
+
+class NoOpSyncManager : ISyncManager {
+
+    override fun startAsync(desiredSynchronizationPort: Int, desiredBasicDataSynchronizationPort: Int, alsoUsePullReplication: Boolean, initializedCallback: (Int) -> Unit) {
+    }
+
+    override fun syncBasicDataWithDevice(deviceId: String, remoteDeviceAddress: String, basicDataSyncPort: Int, syncDone: (Device) -> Unit) {
+    }
+
+    override fun openSynchronizationPort(): Int? {
+        return null
+    }
+
+    override fun closeSynchronizationPort() {
+    }
+
+    override fun startSynchronizationWithDevice(device: DiscoveredDevice) {
+    }
+
+    override fun stopSynchronizationWithDevice(device: DiscoveredDevice) {
+    }
+
+    override fun stop() {
+    }
+
+}
