6766a90445744d426098572e866408c75024fe9f
dankl <dankl@comfylight.com>
2017-12-17 23:26:13 +0100

    Extracted executeActionAfterMeasuringHeight()

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/ContextHelpUtil.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/ContextHelpUtil.kt
@@ -2,9 +2,7 @@ package net.dankito.deepthought.android.views
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
-import android.os.Build
 import android.view.View
-import android.view.ViewTreeObserver
 import android.view.animation.AccelerateInterpolator
 import android.widget.Button
 import android.widget.TextView
@@ -70,17 +68,7 @@ class ContextHelpUtil {
 
     // TODO: animation currently only works on top off screen, not in other places
     private fun animateShowContextHelp(lytContextHelp: View) {
-        if(lytContextHelp.measuredHeight == 0) { // in this case we have to wait till height is determined -> set OnGlobalLayoutListener
-            var layoutListener: ViewTreeObserver.OnGlobalLayoutListener? = null // have to do it that complicated otherwise in OnGlobalLayoutListener we cannot access layoutListener variable
-            layoutListener = ViewTreeObserver.OnGlobalLayoutListener {
-                removeOnGlobalLayoutListener(lytContextHelp, layoutListener)
-
-                animateShowContextHelpAfterMeasuringHeight(lytContextHelp)
-            }
-
-            lytContextHelp.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
-        }
-        else {
+        lytContextHelp.executeActionAfterMeasuringHeight {
             animateShowContextHelpAfterMeasuringHeight(lytContextHelp)
         }
     }
@@ -141,14 +129,5 @@ class ContextHelpUtil {
         yAnimator.start()
     }
 
-
-    private fun removeOnGlobalLayoutListener(lytContextHelp: View, layoutListener: ViewTreeObserver.OnGlobalLayoutListener?) {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
-            lytContextHelp.viewTreeObserver.removeOnGlobalLayoutListener(layoutListener)
-        }
-        else {
-            lytContextHelp.viewTreeObserver.removeGlobalOnLayoutListener(layoutListener)
-        }
-    }
 
 }
--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/ViewExtensions.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/ViewExtensions.kt
@@ -1,7 +1,9 @@
 package net.dankito.deepthought.android.views
 
+import android.os.Build
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewTreeObserver
 
 
 fun View.isTouchInsideView(event: MotionEvent): Boolean {
@@ -31,5 +33,31 @@ fun View.isPointInsideView(x: Float, y: Float): Boolean {
     }
     else {
         return false
+    }
+}
+
+
+fun View.executeActionAfterMeasuringHeight(action: () -> Unit) {
+    if(this.measuredHeight == 0) { // in this case we have to wait till height is determined -> set OnGlobalLayoutListener
+        var layoutListener: ViewTreeObserver.OnGlobalLayoutListener? = null // have to do it that complicated otherwise in OnGlobalLayoutListener we cannot access layoutListener variable
+        layoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+            removeOnGlobalLayoutListener(this, layoutListener)
+
+            action()
+        }
+
+        this.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
+    }
+    else {
+        action()
+    }
+}
+
+private fun removeOnGlobalLayoutListener(lytContextHelp: View, layoutListener: ViewTreeObserver.OnGlobalLayoutListener?) {
+    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+        lytContextHelp.viewTreeObserver.removeOnGlobalLayoutListener(layoutListener)
+    }
+    else {
+        lytContextHelp.viewTreeObserver.removeGlobalOnLayoutListener(layoutListener)
     }
 }
