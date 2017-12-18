d22092943a2d652f4b22b0d759f422ba13325013
dankl <dankl@comfylight.com>
2017-12-18 15:26:07 +0100

    Set correct generic argument

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/ReadLaterArticlesListViewActivity.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/activities/ReadLaterArticlesListViewActivity.kt
@@ -3,9 +3,9 @@ package net.dankito.deepthought.android.activities
 import net.dankito.deepthought.android.R
 import net.dankito.deepthought.android.fragments.EntitiesListViewFragment
 import net.dankito.deepthought.android.fragments.ReadLaterArticlesListView
-import net.dankito.deepthought.android.fragments.ReferencesListView
 
-class ReadLaterArticlesListViewActivity : EntitiesListViewActivityBase<ReferencesListView>(R.string.nav_menu_read_later_articles_title) {
+
+class ReadLaterArticlesListViewActivity : EntitiesListViewActivityBase<ReadLaterArticlesListView>(R.string.nav_menu_read_later_articles_title) {
 
     override fun instantiateListViewFragment(): EntitiesListViewFragment<*> {
         return ReadLaterArticlesListView()
