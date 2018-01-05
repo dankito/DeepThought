d2c2ff51e287e44a164d682f09d754d5fab65e5d
dankl <dankl@comfylight.com>
2018-01-05 22:31:00 +0100

    Forgot to call show(); setting default folder

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/dialogs/FileChooserDialog.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/dialogs/FileChooserDialog.kt
@@ -1,13 +1,14 @@
 package net.dankito.deepthought.android.dialogs
 
 import android.content.Context
+import android.os.Environment
 import yogesh.firzen.filelister.FileListerDialog
 import java.io.File
 
 
 class FileChooserDialog(private val applicationContext: Context) {
 
-    private var lastSelectedFolder: File? = null
+    private var lastSelectedFolder: File = Environment.getExternalStorageDirectory()
 
 
     fun selectFile(initialFolder: File? = lastSelectedFolder, fileSelected: (File) -> Unit) {
@@ -21,6 +22,8 @@ class FileChooserDialog(private val applicationContext: Context) {
 
             fileSelected(file)
         }
+
+        fileListerDialog.show()
     }
 
 }
