ab5e1d9731d0a08e429dd2f3221b72b1698aefaf
dankl <dankl@comfylight.com>
2018-01-14 17:27:09 +0100

    Fixed passing series and editedSourceTitle on to EditSourceDialog

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/routing/JavaFXRouter.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/routing/JavaFXRouter.kt
@@ -89,11 +89,16 @@ class JavaFXRouter(private val mainWindowController: MainWindowController) : IRo
     }
 
     override fun showEditEntryReferenceView(source: Source?, series: Series?, editedSourceTitle: String?) {
-        // TODO: set title when Source is not set
-        mainWindowController.find(EditSourceDialog::class, mapOf(EditSourceDialog::source to (source ?: Source("")))).show(getEditSourceDialogTitle(source?.title ?: ""))
+        val sourceToEdit = source ?: Source("")
+
+        mainWindowController.find(EditSourceDialog::class,
+                mapOf(EditSourceDialog::source to sourceToEdit, EditSourceDialog::series to series, EditSourceDialog::editedSourceTitle to editedSourceTitle)
+        ).show(getEditSourceDialogTitle(sourceToEdit.title, editedSourceTitle))
     }
 
-    private fun getEditSourceDialogTitle(sourceTitle: String) = String.format(FX.messages["edit.source.dialog.title"], sourceTitle)
+    private fun getEditSourceDialogTitle(sourceTitle: String, editedSourceTitle: String? = null): String {
+        return editedSourceTitle ?: String.format(FX.messages["edit.source.dialog.title"], sourceTitle)
+    }
 
 
     override fun showEditSeriesView(series: Series) {
