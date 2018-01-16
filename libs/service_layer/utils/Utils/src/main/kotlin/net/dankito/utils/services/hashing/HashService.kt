bdcadb4128f1f55992ec95c1e03d4117cb336d6d
dankl <dankl@comfylight.com>
2018-01-16 19:59:22 +0100

    Added getFileHash()

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/services/hashing/HashService.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/services/hashing/HashService.kt
@@ -1,6 +1,7 @@
 package net.dankito.utils.services.hashing
 
 
+import java.io.*
 import java.nio.charset.Charset
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
@@ -26,6 +27,33 @@ class HashService {
         messageDigest.update(stringToHashBytes)
 
         return messageDigest.digest()
+    }
+
+    @Throws(NoSuchAlgorithmException::class, IOException::class, FileNotFoundException::class)
+    fun getFileHash(hashAlgorithm: HashAlgorithm, file: File): String {
+        val messageDigest = MessageDigest.getInstance(hashAlgorithm.algorithmName)
+
+        val inputStream = BufferedInputStream(FileInputStream(file))
+        val buffer = ByteArray(2 * 1024)
+
+        var bytesRead = inputStream.read(buffer)
+        while(bytesRead > 0) {
+            messageDigest.update(buffer, 0, bytesRead)
+
+            bytesRead = inputStream.read(buffer)
+        }
+
+        inputStream.close()
+
+        val digestBytes = messageDigest.digest()
+        val hexString = StringBuilder()
+
+        for(i in 0 until digestBytes.size) {
+            hexString.append(Integer.toHexString((0xFF and digestBytes[i].toInt())))
+        }
+
+        return hexString.toString()
+
     }
 
 }
