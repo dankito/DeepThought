72d36ba040a4dbe68c621edb553f902968b22458
dankl <dankl@comfylight.com>
2018-01-17 12:13:25 +0100

    Fixed that other apps can access shared files also on Android 7 and above

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/AndroidApplicationsService.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/AndroidApplicationsService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
+import android.support.v4.content.FileProvider
 import android.webkit.MimeTypeMap
 import android.widget.Toast
 import net.dankito.deepthought.android.R
@@ -21,12 +22,15 @@ class AndroidApplicationsService(private val context: Context, private val fileM
         val absoluteFile = fileManager.getLocalPathForFile(file)
 
         val intent = Intent(ACTION_VIEW)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_GRANT_READ_URI_PERMISSION
 
         try {
             val mimeTypeMap = MimeTypeMap.getSingleton()
             val mimeType = mimeTypeMap.getMimeTypeFromExtension(absoluteFile.extension)
-            intent.setDataAndType(Uri.fromFile(absoluteFile), mimeType)
+
+            // use a FileProvide to give access to file also for devices with Android 7 and above, see https://stackoverflow.com/a/38858040
+            val uri = FileProvider.getUriForFile(context, context.applicationContext.packageName + ".net.dankito.deepthought.android.provider", absoluteFile)
+            intent.setDataAndType(uri, mimeType)
 
             context.startActivity(intent)
         } catch (e: Exception) {
---
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/service/GenericFileProvider.kt
@@ -0,0 +1,8 @@
+package net.dankito.deepthought.android.service
+
+import android.support.v4.content.FileProvider
+
+
+class GenericFileProvider : FileProvider() {
+
+}
