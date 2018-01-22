6b1e04f544ea1b9435a31e445fe23f6653382642
dankl <dankl@comfylight.com>
2018-01-23 00:58:40 +0100

    Extracted CommunicationManagerStarterBase

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/appstart/CommunicationManagerStarter.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/appstart/CommunicationManagerStarter.kt
@@ -1,13 +1,8 @@
 package net.dankito.deepthought.android.appstart
 
 import net.dankito.deepthought.android.di.AppComponent
-import net.dankito.deepthought.communication.ICommunicationManager
-import net.dankito.deepthought.files.FileManager
-import net.dankito.deepthought.files.synchronization.FileServer
+import net.dankito.deepthought.service.communication.CommunicationManagerStarterBase
 import net.dankito.deepthought.service.data.DataManager
-import java.util.*
-import javax.inject.Inject
-import kotlin.concurrent.schedule
 
 
 /**
@@ -17,42 +12,10 @@ import kotlin.concurrent.schedule
  * Therefore after SearchEngine is initialized we give application some time to show initial data
  * before we start communicator classes like @see DevicesDiscoverer, @see IClientCommunicator, @see ISyncManager, ...
  */
-class CommunicationManagerStarter(dataManager: DataManager) {
+class CommunicationManagerStarter(dataManager: DataManager) : CommunicationManagerStarterBase(dataManager) {
 
-    companion object {
-        const val DefaultWaitTimeBeforeStartingCommunicationManagerMillis = 5000L
-    }
-
-
-    @Inject
-    protected lateinit var fileServer: FileServer
-
-    @Inject
-    protected lateinit var fileManager: FileManager // to create FileManager so that listening for changes to FileLinks and FileSyncService is started
-
-    @Inject
-    protected lateinit var communicationManager: ICommunicationManager
-
-
-    init {
-        dataManager.addInitializationListener { dataManagerInitialized() }
-    }
-
-    private fun dataManagerInitialized() {
-        val timer = Timer()
-
-        timer.schedule(DefaultWaitTimeBeforeStartingCommunicationManagerMillis) {
-            startCommunicationManager()
-        }
-    }
-
-
-    private fun startCommunicationManager() {
-        AppComponent.component.inject(this) // and only now create CommunicationManager as now localDevice is loaded from Db
-
-        fileServer.startServerAsync { // start FileServer first so that fileSynchronizationPort get set before communicationManager sends it to synchronized devices
-            communicationManager.startAsync()
-        }
+    override fun injectDependencies() {
+        AppComponent.component.inject(this)
     }
 
 }
