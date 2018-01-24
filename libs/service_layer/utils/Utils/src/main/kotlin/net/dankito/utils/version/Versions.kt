c098979f4a760bf87690025e5968a1bcf0ee406f
dankl <dankl@comfylight.com>
2018-01-24 11:45:48 +0100

    Added change protocol

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Versions.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Versions.kt
@@ -15,4 +15,36 @@ class Versions {
         const val HtmlEditorVersion = 2
     }
 
+
+    /*
+        Change protocol:
+
+
+        Data model:
+
+        - 1 -> 2:
+          Added entity LocalFileInfo
+          FileLink: Added isLocalFile, fileSize, fileLastModified, hashSHA512 and transient localFileInfo. Removed isFolder.
+
+
+        Communication protocol:
+
+        - 1 -> 2:
+          Added fileSynchronizationPort to DiscoveredDevice.
+          Implemented synchronizing files (sending fileSynchronizationPort in messages; added FileServer and FileSyncService)
+
+
+        Search index:
+
+        - 1 -> 2:
+          Added FileLink- and LocalFileInfoIndexWriterAndSearcher. Implemented searching for files and LocalFileInfo.
+
+
+        HTML editor:
+
+        - 1 -> 2:
+          Using now RichTextEditor instead of embedded CKEditor
+
+     */
+
 }
