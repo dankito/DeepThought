615ddf82eb7238f1bc41bcc3d3bd23c97102d647
dankl <dankl@comfylight.com>
2018-01-25 19:31:32 +0100

    Merged constructors

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/clipboard/JavaFXClipboardWatcher.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/clipboard/JavaFXClipboardWatcher.kt
@@ -6,20 +6,17 @@ import net.dankito.utils.UrlUtil
 import java.util.*
 
 
-class JavaFXClipboardWatcher(stage: Stage, private val urlUtil: UrlUtil) {
+class JavaFXClipboardWatcher(stage: Stage, private val urlUtil: UrlUtil, listener: (JavaFXClipboardContent) -> Unit) {
 
     private var sourceOfLastShownPopup: Any? = null
 
     private var clipboardContentChangedExternallyListeners: MutableSet<(JavaFXClipboardContent) -> Unit> = HashSet()
 
 
-    constructor(stage: Stage, urlUtil: UrlUtil, listener: (JavaFXClipboardContent) -> Unit) : this(stage, urlUtil) {
-        addClipboardContentChangedExternallyListener(listener)
-    }
-
-
     init {
         stage.focusedProperty().addListener { _, _, newValue -> checkForChangedClipboardContent(newValue) }
+
+        addClipboardContentChangedExternallyListener(listener)
     }
 
 
