48b58eafa0da1bbbc0d5daab39e953f8d1d79456
dankl <dankl@comfylight.com>
2018-01-25 17:05:13 +0100

    Moved appInitializer.initializeApp() out of setupDI()

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/DeepThoughtJavaFXApplication.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/DeepThoughtJavaFXApplication.kt
@@ -31,6 +31,8 @@ open class DeepThoughtJavaFXApplication : App(MainWindow::class) {
 
         setupDI()
 
+        appInitializer.initializeApp()
+
         super.start(stage)
     }
 
@@ -49,12 +51,7 @@ open class DeepThoughtJavaFXApplication : App(MainWindow::class) {
         CommonComponent.component = component
         AppComponent.component = component
 
-        // DataManager currently initializes itself, so inject DataManager here so that it start asynchronously initializing itself in parallel to creating UI and therefore
-        // speeding app start up a bit.
-        // That's also the reason why LuceneSearchEngine gets injected here so that as soon as DataManager is initialized it can initialize its indices
         component.inject(this)
-
-        appInitializer.initializeApp()
     }
 
     protected open fun createFlavorInstanceProvider(): JavaFXInstanceProvider {
