039d34ec89b761fd9b66975b1f63cf30b327f3e2
dankl <dankl@comfylight.com>
2018-01-30 18:08:00 +0100

    Implemented checking whole hierarchy for ListCells

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/extensions/PickResultExtensions.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/service/extensions/PickResultExtensions.kt
@@ -1,9 +1,23 @@
 package net.dankito.deepthought.javafx.service.extensions
 
 import javafx.scene.control.ListCell
+import javafx.scene.control.ListView
 import javafx.scene.input.PickResult
 
 
 fun <T> PickResult?.findClickedListCell(): ListCell<T>? {
-    return (this?.intersectedNode as? ListCell<T>) ?: this?.intersectedNode?.parent as? ListCell<T>
+    var parent = this?.intersectedNode
+    while(parent != null) {
+        (parent as? ListCell<T>)?.let { listCell ->
+            return listCell
+        }
+
+        if(parent is ListView<*>) {
+            break // we already reached ListView -> we won't find a ListCell
+        }
+
+        parent = parent.parent
+    }
+
+    return null
 }
