93861116eea7769d04a942eabc11989775575340
dankl <dankl@comfylight.com>
2018-01-30 23:59:37 +0100

    Added app colors

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/res/Colors.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/res/Colors.kt
@@ -6,6 +6,11 @@ import javafx.scene.paint.Color
 class Colors {
 
     companion object {
+        // Oliver's green based design
+        val Primary = Color.valueOf("#359e7d")
+        val PrimaryDark = Color.valueOf("#22685d")
+        val Accent = Color.valueOf("#078176")
+
         // argb = (0.949999988079071, 0.529411792755127, 0.8078431487083435, 0.9803921580314636)
         val ClipboardContentPopupBackgroundColor = Color.LIGHTSKYBLUE.deriveColor(0.0, 1.0, 1.0, 0.95)
 
