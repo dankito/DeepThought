f8f28efb12cd638f8ae060535db030d75e97c0f6
dankl <dankl@comfylight.com>
2018-02-15 11:07:57 +0100

    Added getHeaderValue()

--- libs/data_access_layer/network/WebClient/src/main/kotlin/net/dankito/data_access/network/webclient/WebClientResponse.kt
+++ libs/data_access_layer/network/WebClient/src/main/kotlin/net/dankito/data_access/network/webclient/WebClientResponse.kt
@@ -6,4 +6,18 @@ import java.io.InputStream
 data class WebClientResponse(val isSuccessful: Boolean,
                              val headers: Map<String, String>? = null,
                              val error: Exception? = null,
-                             val body: String? = null, val responseStream: InputStream? = null)
+                             val body: String? = null, val responseStream: InputStream? = null) {
+
+    fun getHeaderValue(headerName: String): String? {
+        val headerNameLowerCased = headerName.toLowerCase() // header names are case insensitive, so compare them lower cased
+
+        headers?.keys?.forEach {
+            if(it.toLowerCase() == headerNameLowerCased) {
+                return headers[it]
+            }
+        }
+
+        return null
+    }
+
+}
