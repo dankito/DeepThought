4326eea89883b5bcef945a270408dd968c27cbcc
dankl <dankl@comfylight.com>
2018-02-20 12:03:23 +0100

    Added TODOs

--- libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/DeviceInfo.kt
+++ libs/data_access_layer/network/ClientCommunicator/src/main/kotlin/net/dankito/data_access/network/communication/message/DeviceInfo.kt
@@ -6,8 +6,9 @@ import net.dankito.utils.version.Version
 import net.dankito.utils.version.Versions
 
 
+// TODO: get rid of Version and Versions
 data class DeviceInfo(val id: String, val uniqueDeviceId: String, val name: String, val osType: OsType, val osName: String, val osVersion: String, val description: String,
-                      val appVersion: Version, val dataModelVersion: Int) {
+                      val appVersion: Version, val dataModelVersion: Int) { // TODO: add CommunicationProtocolVersion
 
     companion object {
         fun fromDevice(device: Device): DeviceInfo {
