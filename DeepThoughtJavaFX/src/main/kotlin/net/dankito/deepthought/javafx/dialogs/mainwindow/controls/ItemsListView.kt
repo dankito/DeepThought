be5e0b6e403736b07a50e163078b909cfab20809
dankl <dankl@comfylight.com>
2018-02-22 23:16:40 +0100

    Avoiding superfluous call to Lucene at ItemListView start (will be done anyway as a tag gets automatically selected)

--- DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/controls/ItemsListView.kt
+++ DeepThoughtJavaFX/src/main/kotlin/net/dankito/deepthought/javafx/dialogs/mainwindow/controls/ItemsListView.kt
@@ -19,7 +19,6 @@ import net.dankito.deepthought.ui.IRouter
 import net.dankito.deepthought.ui.presenter.ItemsListPresenter
 import net.dankito.service.data.DeleteEntityService
 import net.dankito.service.search.ISearchEngine
-import net.dankito.service.search.Search
 import net.dankito.utils.ui.IClipboardService
 import net.dankito.utils.ui.IDialogService
 import net.dankito.utils.ui.model.ConfirmationDialogButton
@@ -74,10 +73,6 @@ class ItemsListView : EntitiesListView(), IItemsListViewJavaFX {
         searchBar = ItemsSearchBar(this, presenter, dataManager)
 
         (router as? JavaFXRouter)?.itemsListView = this // TODO: this is bad code design
-
-        searchEngine.addInitializationListener {
-            searchEntities(Search.EmptySearchTerm)
-        }
     }
 
     override fun onUndock() {
