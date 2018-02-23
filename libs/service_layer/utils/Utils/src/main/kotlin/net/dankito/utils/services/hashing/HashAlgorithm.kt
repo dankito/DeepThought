1c46b21592fd681f4f65e7ba9cd6fd9e3d07ef21
dankl <dankl@comfylight.com>
2018-02-23 17:03:37 +0100

    Added comment

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/services/hashing/HashAlgorithm.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/services/hashing/HashAlgorithm.kt
@@ -5,7 +5,7 @@ enum class HashAlgorithm private constructor(val algorithmName: String) {
 
     MD5("MD5"),
     SHA1("SHA1"),
-    SHA224("SHA-224"),
+    SHA224("SHA-224"), // be aware older Androids don't support SHA-224 (but SHA-256 and SHA-512)
     SHA256("SHA-256"),
     SHA384("SHA-384"),
     SHA512("SHA-512");
