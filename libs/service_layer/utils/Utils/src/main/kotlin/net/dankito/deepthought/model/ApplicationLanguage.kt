721b3db276b85dd1d70bc1b4e393154ad0a8320c
dankl <dankl@comfylight.com>
2018-02-23 14:05:03 +0100

    Converted ApplicationLanguage to an enumeration

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/ApplicationLanguage.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/ApplicationLanguage.kt
@@ -1,34 +1,10 @@
 package net.dankito.deepthought.model
 
-import net.dankito.deepthought.model.enums.ExtensibleEnumeration
+
+enum class ApplicationLanguage(val languageKey: String, val languageNameResourceKey: String, val sortOrder: Int) {
 
 
-class ApplicationLanguage : ExtensibleEnumeration {
-
-    companion object {
-        private const val serialVersionUID = -446610923063763955L
-
-        fun getApplicationLanguages(): List<ApplicationLanguage> {
-            val languages = ArrayList<ApplicationLanguage>()
-
-            languages.add(ApplicationLanguage("application.language.english", "en", true, 1))
-            languages.add(ApplicationLanguage("application.language.german", "de", true, 2))
-
-            return languages
-        }
-    }
-
-
-    var languageKey: String = ""
-        private set
-
-
-    private constructor() : this("")
-
-    constructor(name: String) : super(name) {}
-
-    constructor(nameResourceKey: String, languageKey: String, isSystemValue: Boolean, sortOrder: Int) : super(nameResourceKey, isSystemValue, sortOrder) {
-        this.languageKey = languageKey
-    }
+    English("en", "application.language.english", 1),
+    German("de", "application.language.german", 2)
 
 }
