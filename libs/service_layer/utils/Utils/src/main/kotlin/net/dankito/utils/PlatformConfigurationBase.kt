bdc6a5b1282f5dcbbfa3ea8c91b3554f800458d5
dankl <dankl@comfylight.com>
2018-02-23 13:41:06 +0100

    Using FileTypeDefaultFolderName to determine folder for a file

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/PlatformConfigurationBase.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/PlatformConfigurationBase.kt
@@ -1,6 +1,7 @@
 package net.dankito.utils
 
 import net.dankito.deepthought.model.enums.FileType
+import net.dankito.deepthought.model.enums.FileTypeDefaultFolderName
 import java.io.File
 
 
@@ -14,9 +15,19 @@ abstract class PlatformConfigurationBase : IPlatformConfiguration {
 
 
     override fun getDefaultSavePathForFile(filename: String, fileType: FileType): File {
-        val fileFolder = File(getDefaultFilesFolder(), fileType.toString().toLowerCase())
+        val fileFolder = File(getDefaultFilesFolder(), getFolderNameForFileType(fileType))
 
         return File(fileFolder, filename) // use path not absolute path as for Java synchronized files get stored relative to DeepThought.jar -> if DeepThought.jar and files folder get moved, file still gets found
+    }
+
+    private fun getFolderNameForFileType(fileType: FileType): String {
+        return when(fileType) {
+            FileType.Document -> FileTypeDefaultFolderName.Documents.folderName
+            FileType.Image -> FileTypeDefaultFolderName.Images.folderName
+            FileType.Audio -> FileTypeDefaultFolderName.Audios.folderName
+            FileType.Video -> FileTypeDefaultFolderName.Videos.folderName
+            else -> FileTypeDefaultFolderName.OtherFilesFolderName.folderName
+        }
     }
 
     protected fun ensureFolderExists(folder: File): File {
