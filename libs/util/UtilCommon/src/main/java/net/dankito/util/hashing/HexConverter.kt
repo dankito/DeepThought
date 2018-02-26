4526014186a7ab5e199396014ccaa8188338aef1
dankl <dankl@comfylight.com>
2018-02-26 22:41:52 +0100

    Little refactorings

--- libs/util/UtilCommon/src/main/java/net/dankito/util/hashing/HexConverter.kt
+++ libs/util/UtilCommon/src/main/java/net/dankito/util/hashing/HexConverter.kt
@@ -13,7 +13,7 @@ class HexConverter {
         fun byteArrayToHexString(bytes: ByteArray): String {
             val hexChars = CharArray(bytes.size * 2)
 
-            for (j in bytes.indices) {
+            for(j in bytes.indices) {
                 // see http://kotlinlang.org/docs/reference/basic-types.html #Operations
                 val v = bytes[j].and(0xFF.toByte()).toInt()
                 hexChars[j * 2] = hexArray[v ushr(4)]
@@ -28,7 +28,7 @@ class HexConverter {
     fun hexStringToByteArray(hexString: String): ByteArray {
         val bytes = ByteArray(hexString.length / 2)
 
-        for (i in bytes.indices) {
+        for(i in bytes.indices) {
             val index = i * 2
             val v = Integer.parseInt(hexString.substring(index, index + 2), 16)
             bytes[i] = v.toByte()
@@ -38,18 +38,15 @@ class HexConverter {
     }
 
     @JvmOverloads fun byteArrayToHexStringViaStringFormat(bytes: ByteArray, bytesSeparator: String? = ""): String {
-        var bytesSeparator = bytesSeparator
-        if (bytesSeparator == null) {
-            bytesSeparator = ""
-        }
+        val nonNullBytesSeparator = bytesSeparator ?: ""
 
         val stringBuilder = StringBuilder()
 
-        for (singleByte in bytes) {
-            stringBuilder.append(String.format("%02X" + bytesSeparator, singleByte))
+        for(singleByte in bytes) {
+            stringBuilder.append(String.format("%02X" + nonNullBytesSeparator, singleByte))
         }
 
-        stringBuilder.replace(stringBuilder.length - bytesSeparator.length, stringBuilder.length, "")
+        stringBuilder.replace(stringBuilder.length - nonNullBytesSeparator.length, stringBuilder.length, "")
 
         return stringBuilder.toString()
     }
