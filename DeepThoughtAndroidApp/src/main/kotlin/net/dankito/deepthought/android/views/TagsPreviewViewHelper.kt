7e9cb94cb202e09fd1d7eccb5877e7bbdd409d76
dankl <dankl@comfylight.com>
2018-02-26 21:22:58 +0100

    Showing now as a test also on how many items a tag is

--- DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/TagsPreviewViewHelper.kt
+++ DeepThoughtAndroidApp/src/main/kotlin/net/dankito/deepthought/android/views/TagsPreviewViewHelper.kt
@@ -58,7 +58,7 @@ class TagsPreviewViewHelper {
                         else inflater.inflate(R.layout.view_tag, null)
         tagView.tag = TagViewTag
 
-        tagView.txtTagName.text = tag.name
+        tagView.txtTagName.text = tag.displayText
 
         tagView.btnRemoveTagFromItem.visibility = if (showButtonRemoveTag) View.VISIBLE else View.GONE
         tagView.btnRemoveTagFromItem.setOnClickListener { tagRemovedListener?.invoke(tag) }
