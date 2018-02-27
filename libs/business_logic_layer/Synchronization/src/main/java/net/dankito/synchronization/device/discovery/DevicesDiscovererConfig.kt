8c13c288accacf7250fcf05c163737a59a399d5d
dankl <dankl@comfylight.com>
2018-02-27 13:49:45 +0100

    discoveryMessagePrefix now has to be specified, no default value anymore

--- libs/business_logic_layer/Synchronization/src/main/java/net/dankito/synchronization/device/discovery/DevicesDiscovererConfig.kt
+++ libs/business_logic_layer/Synchronization/src/main/java/net/dankito/synchronization/device/discovery/DevicesDiscovererConfig.kt
@@ -1,35 +1,11 @@
 package net.dankito.synchronization.device.discovery
 
 
-class DevicesDiscovererConfig {
-
-    companion object {
-        val DEFAULT_DISCOVERY_MESSAGE_PREFIX = "DT_Disc"
-    }
-
-
-    var localDeviceInfo: String
-
-    var discoverDevicesPort: Int = 0
-
-    var checkForDevicesIntervalMillis: Int = 0
-
-    var discoveryMessagePrefix = DEFAULT_DISCOVERY_MESSAGE_PREFIX
-
-    var listener: DevicesDiscovererListener
-
-
-    constructor(localDeviceInfo: String, discoverDevicesPort: Int, checkForDevicesIntervalMillis: Int, listener: DevicesDiscovererListener) {
-        this.localDeviceInfo = localDeviceInfo
-        this.discoverDevicesPort = discoverDevicesPort
-        this.checkForDevicesIntervalMillis = checkForDevicesIntervalMillis
-        this.listener = listener
-    }
-
-    constructor(localDeviceInfo: String, discoverDevicesPort: Int, checkForDevicesIntervalMillis: Int, discoveryMessagePrefix: String, listener: DevicesDiscovererListener) : this(localDeviceInfo, discoverDevicesPort, checkForDevicesIntervalMillis, listener) {
-        this.discoveryMessagePrefix = discoveryMessagePrefix
-    }
-
+class DevicesDiscovererConfig(val localDeviceInfo: String,
+                              val discoverDevicesPort: Int,
+                              val checkForDevicesIntervalMillis: Int,
+                              val discoveryMessagePrefix: String,
+                              val listener: DevicesDiscovererListener) {
 
     override fun toString(): String {
         return localDeviceInfo
