e40b8b44958ce2af03fbc0d6126767bc86123b12
dankl <dankito@users.noreply.github.com>
2018-03-07 19:24:45 +0100

    Using now the same extraction for Heise Developer as for Heise News

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/HeiseDeveloperArticleExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/HeiseDeveloperArticleExtractor.kt
@@ -1,10 +1,6 @@
 package net.dankito.newsreader.article
 
 import net.dankito.data_access.network.webclient.IWebClient
-import net.dankito.deepthought.model.Item
-import net.dankito.deepthought.model.Source
-import net.dankito.deepthought.model.util.ItemExtractionResult
-import org.jsoup.nodes.Element
 
 
 class HeiseDeveloperArticleExtractor(webClient: IWebClient) : HeiseNewsAndDeveloperArticleExtractorBase(webClient) {
@@ -15,19 +11,6 @@ class HeiseDeveloperArticleExtractor(webClient: IWebClient) : HeiseNewsAndDevelo
 
     override fun canExtractItemFromUrl(url: String): Boolean {
         return isHttpOrHttpsUrlFromHost(url, "www.heise.de/developer/meldung/") || isHttpOrHttpsUrlFromHost(url, "m.heise.de/developer/meldung/")
-    }
-
-
-    override fun parseArticle(extractionResult: ItemExtractionResult, headerElement: Element, articleElement: Element, url: String, title: String) {
-        articleElement.select(".article-content").first()?.let { contentElement ->
-            val item = Item(extractContent(articleElement, url))
-
-            val publishingDate = extractPublishingDate(headerElement)
-            val source = Source(title, url, publishingDate)
-            source.previewImageUrl = makeLinkAbsolute(contentElement.select(".aufmacherbild img").first()?.attr("src") ?: "", url)
-
-            extractionResult.setExtractedContent(item, source)
-        }
     }
 
 }
--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/HeiseNewsAndDeveloperArticleExtractorBase.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/HeiseNewsAndDeveloperArticleExtractorBase.kt
@@ -20,9 +20,6 @@ abstract class HeiseNewsAndDeveloperArticleExtractorBase(webClient: IWebClient) 
     }
 
 
-    abstract protected fun parseArticle(extractionResult: ItemExtractionResult, headerElement: Element, articleElement: Element, url: String, title: String)
-
-
     override fun parseHtmlToArticle(extractionResult: ItemExtractionResult, document: Document, url: String) {
         document.body().select("article").first()?.let { article ->
             getReadAllOnOnePageUrl(article, url)?.let { allOnOnePageUrl ->
@@ -57,6 +54,45 @@ abstract class HeiseNewsAndDeveloperArticleExtractorBase(webClient: IWebClient) 
         }
 
         return null
+    }
+
+
+    protected open fun parseArticle(extractionResult: ItemExtractionResult, headerElement: Element, articleElement: Element, url: String, title: String) {
+        articleElement.select(".meldung_wrapper").first()?.let { contentElement ->
+            parseMeldungWrapperArticle(extractionResult, headerElement, articleElement, contentElement, url, title)
+            return
+        }
+
+        articleElement.select(".article-content").first()?.let { articleContentElement ->
+            parseArticleContentArticle(extractionResult, headerElement, articleContentElement, url, title)
+        }
+    }
+
+
+    // new version
+    protected fun parseArticleContentArticle(extractionResult: ItemExtractionResult, headerElement: Element, articleContentElement: Element, url: String, title: String) {
+        val source = Source(title, url, extractPublishingDate(headerElement))
+        articleContentElement.select(".aufmacherbild img").first()?.let { previewImageElement ->
+            source.previewImageUrl = makeLinkAbsolute(previewImageElement.attr("src"), url)
+        }
+
+        cleanContentElement(articleContentElement)
+
+        makeLinksAbsolute(articleContentElement, url)
+
+        extractionResult.setExtractedContent(Item(articleContentElement.outerHtml()), source)
+    }
+
+
+    // old version
+    protected fun parseMeldungWrapperArticle(extractionResult: ItemExtractionResult, headerElement: Element, articleElement: Element, contentElement: Element, url: String, title: String) {
+        val item = Item(extractContent(articleElement, url))
+
+        val publishingDate = extractPublishingDate(headerElement)
+        val source = Source(title, url, publishingDate)
+        source.previewImageUrl = makeLinkAbsolute(contentElement.select(".aufmacherbild img").first()?.attr("src") ?: "", url)
+
+        extractionResult.setExtractedContent(item, source)
     }
 
 
