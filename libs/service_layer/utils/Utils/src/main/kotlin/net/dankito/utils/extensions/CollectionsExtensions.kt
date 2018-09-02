e8d9d269f8d4f633a706cca20acb95d560c88865
dankl <dankito@users.noreply.github.com>
2018-09-02 23:38:41 +0200

    Using now sortedByStrings() from JavaUtils

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/extensions/CollectionsExtensions.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/extensions/CollectionsExtensions.kt
@@ -1,8 +1,8 @@
 package net.dankito.utils.extensions
 
 import net.dankito.deepthought.model.Tag
+import net.dankito.utils.extensions.sortedByStrings
 import java.text.Collator
-import java.util.*
 
 
 class CollectionsExtensions {
@@ -15,14 +15,6 @@ class CollectionsExtensions {
     }
 }
 
-
-/**
- * Standard sortedBy() function doesn't take characters like German Umlaute into consideration (so that e.g. Ärzte is ordered after Zucker)
- * -> use a Collator with at least strenght of Collator.SECONDARY
- */
-inline fun <T> Iterable<T>.sortedByStrings(crossinline selector: (T) -> String): List<T> {
-    return sortedWith(kotlin.Comparator { o1, o2 -> CollectionsExtensions.collator.compare(selector(o1), selector(o2)) })
-}
 
 fun Iterable<Tag>.sorted(): List<Tag> {
     return this.filterNotNull().sortedByStrings { it.name }
