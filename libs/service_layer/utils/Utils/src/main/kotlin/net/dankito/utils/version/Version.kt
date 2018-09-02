f895056bdd36eacfbb610d7540a509e1cbb364bf
dankl <dankito@users.noreply.github.com>
2018-09-02 23:03:41 +0200

    Using now Version from JavaUtils

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/version/Version.kt
+++
@@ -1,7 +0,0 @@
-package net.dankito.utils.version
-
-
-class Version(val major: Int, val minor: Int, val patch: Int = 0, val build: String? = null) {
-
-    private constructor() : this(0, 0, 0) // for Jackson
-}
