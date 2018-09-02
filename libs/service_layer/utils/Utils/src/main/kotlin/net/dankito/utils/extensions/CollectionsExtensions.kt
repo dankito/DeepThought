f5bba3d8f4c162c79f34bcb51e88fb5d459963c3
dankl <dankito@users.noreply.github.com>
2018-09-03 00:32:02 +0200

    Using now didCollectionChange() and collator from JavaUtils

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/extensions/CollectionsExtensions.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/utils/extensions/CollectionsExtensions.kt
@@ -1,19 +1,6 @@
 package net.dankito.utils.extensions
 
 import net.dankito.deepthought.model.Tag
-import net.dankito.utils.extensions.sortedByStrings
-import java.text.Collator
-
-
-class CollectionsExtensions {
-    companion object {
-        val collator = Collator.getInstance()
-
-        init {
-            collator.strength = Collator.IDENTICAL
-        }
-    }
-}
 
 
 fun Iterable<Tag>.sorted(): List<Tag> {
@@ -22,15 +9,4 @@ fun Iterable<Tag>.sorted(): List<Tag> {
 
 fun Iterable<Tag>.toSortedString(): String {
     return this.sorted().joinToString { it.name }
-}
-
-
-fun <T> Collection<T>.didCollectionChange(collectionToCompareTo: Collection<T>): Boolean {
-    if(this.size != collectionToCompareTo.size) {
-        return true
-    }
-
-    val copy = ArrayList(collectionToCompareTo)
-    copy.removeAll(this)
-    return copy.size > 0
 }
