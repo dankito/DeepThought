7edd88747ba57968c7930105fb24d7a85769898a
dankl <dankito@users.noreply.github.com>
2018-09-02 22:43:16 +0200

    Moved compareTo() to bottom

--- libs/util/FaviconExtractor/src/main/kotlin/net/dankito/faviconextractor/Size.kt
+++ libs/util/FaviconExtractor/src/main/kotlin/net/dankito/faviconextractor/Size.kt
@@ -3,17 +3,17 @@ package net.dankito.faviconextractor
 
 data class Size(val width: Int, val height: Int) : Comparable<Size> {
 
+    fun isSquare(): Boolean {
+        return width == height
+    }
+
+
     override fun compareTo(other: Size): Int {
         if(width == other.width) {
             return height.compareTo(other.height)
         }
 
         return width.compareTo(other.width)
-    }
-
-
-    fun isSquare(): Boolean {
-        return width == height
     }
 
 
