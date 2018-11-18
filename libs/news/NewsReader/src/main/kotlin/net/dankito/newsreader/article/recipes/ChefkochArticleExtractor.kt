fa03f22e06be9cd345886b5034beaf18bc0484a9
dankl <dankito@users.noreply.github.com>
2018-11-18 18:05:14 +0100

    Fixed removing recipe statistics (flex--desktop)

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/recipes/ChefkochArticleExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/recipes/ChefkochArticleExtractor.kt
@@ -1,10 +1,10 @@
 package net.dankito.newsreader.article.recipes
 
-import net.dankito.utils.web.client.IWebClient
 import net.dankito.deepthought.model.Item
 import net.dankito.deepthought.model.Source
 import net.dankito.deepthought.model.util.ItemExtractionResult
 import net.dankito.newsreader.article.ArticleExtractorBase
+import net.dankito.utils.web.client.IWebClient
 import org.jsoup.nodes.Document
 
 
@@ -37,7 +37,7 @@ class ChefkochArticleExtractor(webClient: IWebClient) : ArticleExtractorBase(web
 //            content += contentWrapper.select("#slideshow").first()?.outerHtml() ?: ""
 
             contentWrapper.select("div.main-content").first()?.let { mainContent ->
-                mainContent.select("#text-ads-rezeptbild, .how2video-container, .mobile-only, .order-online, .recipe2shoppinglist," +
+                mainContent.select(".flex--desktop, #text-ads-rezeptbild, .how2video-container, .mobile-only, .order-online, .recipe2shoppinglist," +
                         "#text-ads-unter-zubereitung, #recipe-com-user-footer, .sfs-esi-positioning-container, #rezeptvideos").remove()
 
                 // TODO: may also get videos to work, remove #rezeptvideos then from above's content cleaning
