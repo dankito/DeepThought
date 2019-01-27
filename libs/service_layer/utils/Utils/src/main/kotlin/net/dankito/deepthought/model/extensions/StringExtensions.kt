52a87e0831e233613a6a6369cff1820fcabb0ecb
dankl <dankito@users.noreply.github.com>
2019-01-27 20:47:52 +0100

    Removing <figure>s (and therefore figure captions) from plain text

--- libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/extensions/StringExtensions.kt
+++ libs/service_layer/utils/Utils/src/main/kotlin/net/dankito/deepthought/model/extensions/StringExtensions.kt
@@ -5,7 +5,11 @@ import org.jsoup.Jsoup
 
 fun String?.getPlainTextForHtml(): String {
     if(this != null) {
-        val textWithTags = Jsoup.parseBodyFragment(this).text()
+        val document = Jsoup.parseBodyFragment(this)
+
+        document.select("figure").remove() // remove figure captions as they often don't print nicely in plain text
+
+        val textWithTags = document.text()
         return Jsoup.parse(textWithTags).text() // don't know why but html has to be parsed twice in this way to remove tags like <img>, see https://github.com/jhy/jsoup/issues/125
     }
 
