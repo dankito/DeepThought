dd0b4c3ed484d36282e1d51022f141496d875158
dankito <git@dankito.net>
2023-01-22 02:27:33 +0100

    Updated to new Freitag home page structure

--- libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/DerFreitagArticleSummaryExtractor.kt
+++ libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/DerFreitagArticleSummaryExtractor.kt
@@ -29,47 +29,43 @@ class DerFreitagArticleSummaryExtractor(webClient: IWebClient) : ArticleSummaryE
         return ArticleSummary(extractArticles(document))
     }
 
-    private fun extractArticles(document: Document): List<ArticleSummaryItem> {
-        val articles = mutableListOf<ArticleSummaryItem>()
-
-        articles.addAll(extractArticleCards(document))
-
-        return articles
+    private fun extractArticles(document: Document) = mutableListOf<ArticleSummaryItem>().apply {
+        extractBookOfTheWeek(document)?.let { add(it) }
+        addAll(extractArticleCards(document))
     }
 
-    private fun extractArticleCards(document: Document): Collection<ArticleSummaryItem> {
-        return document.body().select(".c-article-card").map { mapCardElementToArticleSummaryItem(it) }.filterNotNull()
-    }
+    private fun extractArticleCards(document: Document) = document.select(".c-article-card2")
+        .mapNotNull { mapCardElementToArticleSummaryItem(it) }
 
-    private fun mapCardElementToArticleSummaryItem(articleCardElement: Element): ArticleSummaryItem? {
-        articleCardElement.select(".c-article-card__title a").first()?.let { titleAnchor ->
-            val articleUrl = titleAnchor.attr("href")
-            val item = ArticleSummaryItem(articleUrl, titleAnchor.text().trim(), getExtractorClassForUrl(articleUrl))
+    private fun mapCardElementToArticleSummaryItem(card2Element: Element): ArticleSummaryItem? =
+        card2Element.selectFirst(".export-article-card-url")?.let { urlElement ->
+            val articleUrl = urlElement.attr("href")
+            val title = urlElement.text()
+            val summary = card2Element.selectFirst(".c-article-card2__text")?.let { summaryElement ->
+                val categoryElement = summaryElement.selectFirst("strong")
+                if (categoryElement != null) {
+                    val category = categoryElement.text().trim()
+                    categoryElement.remove()
+                    "$category: ${summaryElement.text().trim()}"
+                } else summaryElement.text().trim()
+            } ?: ""
+            val previewImageUrl = card2Element.selectFirst("img")?.attr("src")
 
-            articleCardElement.select(".c-article-card__teaser").first()?.let { teaserElement ->
-                item.summary = teaserElement.text().trim()
-
-                articleCardElement.select(".c-article-card__source").first()?.let { source ->
-                    val url = source.attr("href")
-                    if(url.contains(".freitag.de/") == false) {
-                        item.summary += "\r\n" + urlUtil.getHostName(url)
-                    }
-                }
-            }
-
-            articleCardElement.select(".c-article-card__image img").first()?.let { previewImageElement ->
-                item.previewImageUrl = previewImageElement.attr("data-src")
-
-                if(item.previewImageUrl == null) {
-                    item.previewImageUrl = previewImageElement.attr("src")
-                }
-            }
-
-            return item
+            ArticleSummaryItem(articleUrl, title, getExtractorClassForUrl(articleUrl), summary, previewImageUrl)
         }
 
-        return null
-    }
+    private fun extractBookOfTheWeek(document: Document): ArticleSummaryItem? =
+        document.body().selectFirst("#buch")?.let { bookElement ->
+            val articleUrl = bookElement.selectFirst("a")?.attr("href")
+            val author = bookElement.selectFirst(".c-advertorial-info__author")?.text()
+            val bookTitle = bookElement.selectFirst(".c-advertorial-info__title")?.text()
+            if (articleUrl.isNullOrBlank() == false && author.isNullOrBlank() == false && bookTitle.isNullOrBlank() == false) {
+                val summary = bookElement.selectFirst(".c-advertorial-info__description")?.text()?.trim() ?: ""
+                val previewImageUrl = bookElement.selectFirst("img")?.attr("href")
+
+                ArticleSummaryItem(articleUrl, "$author - $bookTitle", getExtractorClassForUrl(articleUrl), summary, previewImageUrl)
+            } else null
+        }
 
     private fun getExtractorClassForUrl(articleUrl: String): Class<out IArticleExtractor>? {
         if(articleUrl.contains(".freitag.de/")) {
