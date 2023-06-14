87be0b140e4859062cfb7160a06ff1c1ce7ac7ec
dankito <git@dankito.net>
2023-06-14 15:27:02 +0200

    Implemented getting smallest preview image url to reduce bandwidth

--- libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/SueddeutscheArticleSummaryExtractor.kt
+++ libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/SueddeutscheArticleSummaryExtractor.kt
@@ -13,6 +13,10 @@ import org.jsoup.nodes.Element
 
 class SueddeutscheArticleSummaryExtractor(webClient: IWebClient) : ArticleSummaryExtractorBase(webClient) {
 
+    companion object {
+        private val RectQueryParameterRegex = Regex("rect=[0-9,]+[&amp;|amp]")
+    }
+
     override fun getName(): String {
         return "SZ"
     }
@@ -50,7 +54,7 @@ class SueddeutscheArticleSummaryExtractor(webClient: IWebClient) : ArticleSummar
                 extractTitle(titleElement, dataHydrationElement)?.let { title ->
                     val articleUrl = makeLinkAbsolute(articleLinkElement.attr("href"), siteUrl)
                     val summary = articleElement.selectFirst("[data-manual='teaser-text']")?.text()?.trim() ?: ""
-                    val previewImageUrl = articleElement.selectFirst("img[data-manual='teaser-image']")?.attr("src")?.let { makeLinkAbsolute(it, siteUrl) } // TODO: are there also smaller images?
+                    val previewImageUrl = getPreviewImageUrl(articleElement, siteUrl)
                     val item = ArticleSummaryItem(articleUrl, title, getArticleExtractorClass(articleUrl), summary, previewImageUrl)
 
                     articleElement.selectFirst(".sz-teaser__summary")?.let { summaryElement ->
@@ -95,6 +99,33 @@ class SueddeutscheArticleSummaryExtractor(webClient: IWebClient) : ArticleSummar
     private fun isSzPlusArticle(titleElement: Element, dataHydrationElement: Element?) =
         titleElement.selectFirst("svg")?.text()?.contains("SZ Plus") == true ||
                 dataHydrationElement?.attr("data-hydration-component-name") == "SZPlusGroup"
+
+    private fun getPreviewImageUrl(articleElement: Element, siteUrl: String): String? =
+        articleElement.selectFirst("img[data-manual='teaser-image']")?.let { imgElement ->
+            var previewImageUrl = imgElement.attr("src") // preselected source, in most cases the largest image
+
+            // try to find a smaller image in srcSet
+            val srcSet = imgElement.attr("srcSet")
+            if (srcSet.isNotBlank()) {
+                srcSet.split(", ")
+                    .mapNotNull {
+                        val urlToSize = it.split( ' ')
+                        if (urlToSize.size > 1) {
+                            urlToSize[1].replace("w", "").toIntOrNull() to urlToSize[0] // map to: size to url
+                        } else null
+                    }
+                    .sortedBy { it.first } // sort by size ascending
+                    .firstOrNull { it.first != null } // select smallest image
+                    ?.second // select url
+                    ?.let { smallestImageUrl ->
+                        // remove "rect=0,0,1427,802&amp;" as otherwise always a large image gets returned
+                        previewImageUrl = RectQueryParameterRegex.replace(smallestImageUrl, "")
+                    }
+            }
+
+            makeLinkAbsolute(previewImageUrl, siteUrl)
+        }
+
 
 
     private fun getArticleExtractorClass(articleUrl: String): Class<out ArticleExtractorBase> {
