dd1afc873d3127f8001c94ce7dd42f9212c32173
dankito <git@dankito.net>
2024-11-06 19:53:41 +0100

    Implemented parsing slide shows

--- libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/KickerArticleExtractor.kt
+++ libs/news/NewsReader/src/main/kotlin/net/dankito/newsreader/article/KickerArticleExtractor.kt
@@ -6,8 +6,20 @@ import net.dankito.deepthought.model.util.ItemExtractionResult
 import net.dankito.utils.extensions.attrOrNull
 import net.dankito.utils.web.client.IWebClient
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.slf4j.LoggerFactory
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class KickerArticleExtractor(webClient: IWebClient) : ArticleExtractorBase(webClient) {
+
+    companion object {
+        private val kickerDateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+
+        private val log = LoggerFactory.getLogger(ArticleExtractorBase::class.java)
+    }
+
 
     override fun getName() = "Kicker"
 
@@ -24,11 +36,47 @@ class KickerArticleExtractor(webClient: IWebClient) : ArticleExtractorBase(webCl
                     val summary = contentElement.selectFirst(".kick__article__teaser")?.text() ?: ""
                     val previewImage = contentElement.selectFirst(".kick__article__picture--teaser, .kick__article__picture")
                         ?.selectFirst("img")?.attrOrNull("src")
+                    val publishingDate = parseDate(document)
 
-                    extractionResult.setExtractedContent(Item(content.outerHtml()), Source(title, url, previewImageUrl = previewImage, subTitle = subTitle))
+                    extractionResult.setExtractedContent(Item(content.outerHtml()), Source(title, url, publishingDate, previewImage, subTitle))
                 }
             }
         }
+
+        document.body().selectFirst(".kick__slideshow__view")?.let { slideshowElement ->
+            extractSlideshowArticle(extractionResult, document, url, slideshowElement)
+        }
     }
+
+    private fun extractSlideshowArticle(extractionResult: ItemExtractionResult, document: Document, url: String, slideshowElement: Element) {
+        val title = slideshowElement.selectFirst(".kick__slideshow__view__title")?.text() ?: ""
+        val subTitle = slideshowElement.selectFirst(".kick__slideshow__view__subtitle p")?.text() ?: ""
+        val previewImage = slideshowElement.selectFirst(".kick__slideshow__view-picture picture img")?.attrOrNull("src")
+        val publishingDate = parseDate(document)
+
+        extractionResult.setExtractedContent(Item(slideshowElement.outerHtml()), Source(title, url, publishingDate, previewImage, subTitle))
+    }
+
+    private fun parseDate(document: Document): Date? =
+        try {
+            var dateString = document.head().selectFirst("meta[name='Date']")?.attrOrNull("content")
+            if (dateString != null) {
+                val timeZoneStartIndex = dateString.indexOf('+')
+                if (timeZoneStartIndex > -1) {
+                    val indexOfColonInTimeZone = dateString.indexOf(':', timeZoneStartIndex)
+                    if (indexOfColonInTimeZone > -1) {
+                        // time zone is state as '+01:00', but SimpleDateFormat expects '+0100' -> remove colon
+                        dateString = dateString.removeRange(indexOfColonInTimeZone, indexOfColonInTimeZone + 1)
+                    }
+                }
+
+                kickerDateFormat.parse(dateString)
+            } else {
+                null
+            }
+        } catch (e: Throwable) {
+            log.error("Could not parse Kicker date", e)
+            null
+        }
 
 }
