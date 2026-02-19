b6b9b721be972d74178857a59124c67d404a9f14
dankito <git@dankito.net>
2026-02-19 01:56:56 +0100

    Fixed detecting heise+ articles

--- libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/HeiseNewsAndDeveloperArticleSummaryExtractorBase.kt
+++ libs/news/ArticleSummaryExtractors/src/main/kotlin/net/dankito/newsreader/summary/HeiseNewsAndDeveloperArticleSummaryExtractorBase.kt
@@ -149,7 +149,8 @@ abstract class HeiseNewsAndDeveloperArticleSummaryExtractorBase(webClient: IWebC
 
     private fun isHeisePlusArticle(articleElement: Element): Boolean {
         return articleElement.selectFirst("footer svg[width=\"78\"][height=\"24\"][role=\"img\"]") != null ||
-                articleElement.selectFirst("header [data-component=\"TeaserHeadline\"] svg") != null
+                articleElement.selectFirst("header [data-component=\"TeaserHeadline\"] svg") != null ||
+                articleElement.selectFirst("header [data-component=\"TeaserHeadline\"] img[src=\"/icons/svg/logos/svg/heise_plus_blue.svg\"]") != null
     }
 
 
